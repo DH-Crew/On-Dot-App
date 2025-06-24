7f004a82c8d8932fa6a51fd33f4289bad0a75910
dogmania <hj296914@gmail.com>
2025-06-25 01:18:32 +0900

    :recycle: Refactor: 자바 17로 변경

--- build-logic/src/main/java/com/ondot/build_logic/AndroidApplicationPlugin.kt
+++ build-logic/src/main/java/com/ondot/build_logic/AndroidApplicationPlugin.kt
@@ -6,16 +6,16 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.findByType
 import org.gradle.kotlin.dsl.getByType
 
 class AndroidApplicationPlugin : Plugin<Project> {
-    override fun apply(target: Project) = with(target) {
-        plugins.apply("com.android.application")
+    override fun apply(project: Project) = with(project) {
+        pluginManager.apply("com.android.application")
+
+        val libs = extensions.findByType<VersionCatalogsExtension>()!!.named("libs")
 
         extensions.configure<ApplicationExtension> {
-            val libs = libsCatalogOrNull()?.named("libs")
-                ?: error("libs version catalog not found")
-
             namespace = "com.dh.ondot"
             compileSdk = libs.findVersion("android-compileSdk").get().requiredVersion.toInt()
 
@@ -27,13 +27,9 @@ class AndroidApplicationPlugin : Plugin<Project> {
                 versionName = "1.0"
             }
 
-            buildTypes {
-                getByName("release") {
-                    isMinifyEnabled = false
-                }
-            }
-
             packaging.resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
+
+            buildTypes.getByName("release").isMinifyEnabled = false
 
             compileOptions {
                 sourceCompatibility = JavaVersion.VERSION_17
