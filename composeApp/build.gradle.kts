f6c694b527b3e78f270e0bedaf3f591a0bbd26b7
dogmania <hj296914@gmail.com>
2025-06-25 17:33:47 +0900

    :sparkles: Feat: CocoaPods 설정

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,6 +1,5 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
     id("ondot.compose.app")
@@ -8,9 +7,21 @@ plugins {
 
 kotlin {
     androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_17)
         }
     }
+}
+
+val syncFramework = tasks.register<Sync>("syncFramework") {
+    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
+    val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
+    val targetName = if (sdkName.startsWith("iphoneos")) "iosArm64" else "iosX64"
+
+    val nativeTarget = kotlin.targets.getByName(targetName) as KotlinNativeTarget
+    val framework = nativeTarget.binaries.getFramework(mode)
+
+    dependsOn(framework.linkTaskProvider)
+    from({ framework.outputDirectory })
+    into(layout.buildDirectory.dir("xcode-frameworks"))
 }
