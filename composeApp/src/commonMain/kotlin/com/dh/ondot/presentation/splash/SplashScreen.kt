9ed77abc12ceed21b6c4fb1c47e733b1381a3f90
dogmania <hj296914@gmail.com>
2025-06-27 00:46:20 +0900

    :sparkles: Feat: Splash 화면 구현

---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/splash/SplashScreen.kt
@@ -0,0 +1,59 @@
+package com.dh.ondot.presentation.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.dh.ondot.di.provideTokenProvider
+import io.github.alexzhirkevich.compottie.Compottie
+import io.github.alexzhirkevich.compottie.LottieCompositionSpec
+import io.github.alexzhirkevich.compottie.animateLottieCompositionAsState
+import io.github.alexzhirkevich.compottie.rememberLottieComposition
+import io.github.alexzhirkevich.compottie.rememberLottiePainter
+import ondot.composeapp.generated.resources.Res
+
+@Composable
+fun SplashScreen(
+    viewModel: SplashViewModel = viewModel {
+        SplashViewModel(
+            tokenProvider = provideTokenProvider()
+        )
+    }
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val composition by rememberLottieComposition {
+        LottieCompositionSpec.JsonString(Res.readBytes("files/lotties/splash.json").decodeToString())
+    }
+    val progress by animateLottieCompositionAsState(
+        composition,
+        iterations = Compottie.IterateForever
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xff1B1B1B)),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = rememberLottiePainter(
+                composition = composition,
+                progress = { progress }
+            ),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 92.dp)
+        )
+    }
+}
