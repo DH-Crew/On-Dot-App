ed047cb28a24488232a7bb61b6816aeed12addfe
dogmania <hj296914@gmail.com>
2025-06-27 00:42:37 +0900

    :sparkles: Feat: Hotreload 의존성 추가

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
     id("ondot.compose.app")
+    alias(libs.plugins.composeHotReload)
+    alias(libs.plugins.kotlinCocoapods)
 }
 
 kotlin {
@@ -11,17 +13,17 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_17)
         }
     }
-}
 
-val syncFramework = tasks.register<Sync>("syncFramework") {
-    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
-    val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
-    val targetName = if (sdkName.startsWith("iphoneos")) "iosArm64" else "iosX64"
+    cocoapods {
+        version = "1.0.0"
 
-    val nativeTarget = kotlin.targets.getByName(targetName) as KotlinNativeTarget
-    val framework = nativeTarget.binaries.getFramework(mode)
-
-    dependsOn(framework.linkTaskProvider)
-    from({ framework.outputDirectory })
-    into(layout.buildDirectory.dir("xcode-frameworks"))
+        summary = "Compose Multiplatform Shared UI"
+        homepage = "https://github.com/DH-Crew/On-Dot-App"
+        ios.deploymentTarget = "14.1"
+        framework {
+            baseName = "composeApp"
+            isStatic = true
+            binaryOption("bundleId", "com.dh.ondot")
+        }
+    }
 }
