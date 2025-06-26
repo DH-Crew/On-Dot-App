9d1b183f93b5ef1e7f1879c51803c0e53116fd4a
dogmania <hj296914@gmail.com>
2025-06-27 00:55:27 +0900

    :recycle: Refactor: ApplicationContext 접근 로직 예외 구현

--- composeApp/src/androidMain/kotlin/com/dh/ondot/di/PlatformDependencies.android.kt
+++ composeApp/src/androidMain/kotlin/com/dh/ondot/di/PlatformDependencies.android.kt
@@ -3,4 +3,8 @@ package com.dh.ondot.di
 import com.dh.ondot.network.TokenProvider
 import com.dh.ondot.util.AppContextHolder
 
-actual fun provideTokenProvider(): TokenProvider = TokenProvider(context = AppContextHolder.context)
+actual fun provideTokenProvider(): TokenProvider {
+    val context = runCatching { AppContextHolder.context }
+        .getOrElse { error("AppContextHolder.context가 아직 초기화되지 않았습니다.") }
+    return TokenProvider(context = context)
+}
