65b955d3a0cc82345bb62ee645becce32dbd2367
dogmania <hj296914@gmail.com>
2025-06-27 00:42:01 +0900

    :sparkles: Feat: Compottie 의존성 추가

--- build-logic/src/main/java/com/ondot/build_logic/ComposeMultiplatformPlugin.kt
+++ build-logic/src/main/java/com/ondot/build_logic/ComposeMultiplatformPlugin.kt
@@ -2,11 +2,14 @@ package com.ondot.build_logic
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.MinimalExternalModuleDependency
+import org.gradle.api.artifacts.ModuleDependency
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByName
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.creating
+import org.gradle.kotlin.dsl.exclude
 import org.gradle.kotlin.dsl.findByType
 import org.gradle.kotlin.dsl.getting
 import org.gradle.kotlin.dsl.provideDelegate
@@ -28,6 +31,7 @@ class ComposeMultiplatformPlugin : Plugin<Project> {
         project.extensions.configure<KotlinMultiplatformExtension> {
             with(sourceSets) {
                 val commonMain = maybeCreate("commonMain")
+                commonMain.resources.srcDirs("src/commonMain/resources")
                 commonMain.dependencies {
                     implementation(libs.findLibrary("compose-runtime").get())
                     implementation(libs.findLibrary("compose-foundation").get())
@@ -42,7 +46,11 @@ class ComposeMultiplatformPlugin : Plugin<Project> {
                     implementation(libs.findLibrary("ktor-client-logging").get())
                     implementation(libs.findLibrary("viewmodel").get())
                     implementation(libs.findLibrary("navigation").get())
+                    implementation(libs.findLibrary("compottie").get())
+                    implementation(libs.findLibrary("compottie-dot").get())
+                    implementation(libs.findLibrary("compottie-network").get())
                 }
+
                 val androidMain = maybeCreate("androidMain")
                 androidMain.dependencies {
                     implementation(libs.findLibrary("androidx-activity-compose").get())
@@ -54,6 +62,7 @@ class ComposeMultiplatformPlugin : Plugin<Project> {
                     implementation(libs.findLibrary("datastore-preferences").get())
                     implementation(libs.findLibrary("ktor-client-okhttp").get())
                 }
+                
                 val iosMain = maybeCreate("iosMain")
                 iosMain.dependencies {
                     implementation(libs.findLibrary("ktor-client-darwin").get())
@@ -61,7 +70,26 @@ class ComposeMultiplatformPlugin : Plugin<Project> {
             }
         }
 
-        // 4) ui-tooling debugImplementation 으로 추가
+        project.configurations.configureEach {
+            resolutionStrategy.eachDependency {
+                if (requested.group == "org.jetbrains.kotlinx" &&
+                    requested.name  == "kotlinx-io-core") {
+                    useVersion("0.3.3")
+                    because("Align io-core with Ktor to avoid ByteReadPacket symbol clash")
+                }
+                if (requested.group == "org.jetbrains.kotlinx" &&
+                    requested.name == "kotlinx-io-bytestring") {
+                    useVersion("0.3.3")
+                }
+            }
+        }
+
+        project.configurations.configureEach {
+            resolutionStrategy.eachDependency {
+                if (requested.group == "io.ktor") useVersion("2.3.10")
+            }
+        }
+
         project.dependencies.add(
             "debugImplementation",
             libs.findLibrary("compose-uiTooling").get()
