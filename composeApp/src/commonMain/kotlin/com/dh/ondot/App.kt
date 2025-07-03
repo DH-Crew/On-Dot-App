ddc863d0dadf3591d0d502bd781ec1cd2d4613c3
dogmania <hj296914@gmail.com>
2025-07-04 03:44:05 +0900

    :sparkles: Feat: OnDotTheme 적용

--- composeApp/src/commonMain/kotlin/com/dh/ondot/App.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/App.kt
@@ -2,22 +2,34 @@ package com.dh.ondot
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.dh.ondot.presentation.login.LoginScreen
 import com.dh.ondot.presentation.splash.SplashScreen
+import com.dh.ondot.presentation.ui.theme.OnDotTheme
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun App() {
-    MaterialTheme {
-        SplashScreen()
+    OnDotTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            LoginScreen()
+        }
     }
 }
