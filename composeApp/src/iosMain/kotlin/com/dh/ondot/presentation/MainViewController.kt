49ef320b44d7f3358992786a0543dff8669681cb
dogmania <hj296914@gmail.com>
2025-07-04 03:44:27 +0900

    :recycle: Refactor: 파일 위치 변경

--- composeApp/src/iosMain/kotlin/com/dh/ondot/presentation/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/dh/ondot/presentation/MainViewController.kt
@@ -1,5 +1,6 @@
-package com.dh.ondot
+package com.dh.ondot.presentation
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.dh.ondot.App
 
 fun MainViewController() = ComposeUIViewController { App() }
