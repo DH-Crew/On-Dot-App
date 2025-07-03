2ca7ca9d43f5345f16d5d94093fb71e8721fa700
dogmania <hj296914@gmail.com>
2025-07-04 03:47:48 +0900

    :sparkles: Feat: LoginScreen 구현

---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/login/LoginScreen.kt
@@ -0,0 +1,94 @@
+package com.dh.ondot.presentation.login
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.dh.ondot.di.provideTokenProvider
+import com.dh.ondot.domain.model.enums.ButtonType
+import com.dh.ondot.getPlatform
+import com.dh.ondot.presentation.ui.components.OnDotButton
+import com.dh.ondot.presentation.ui.theme.IOS
+import com.dh.ondot.presentation.ui.theme.KAKAO_LOGIN_BUTTON_TEXT
+import com.dh.ondot.presentation.ui.theme.OnDotColor
+import ondot.composeapp.generated.resources.Res
+import ondot.composeapp.generated.resources.ic_login
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun LoginScreen(
+    viewModel: LoginViewModel = viewModel {
+        LoginViewModel(tokenProvider = provideTokenProvider())
+    }
+) {
+    LoginContent(
+        onKakaoLogin = { viewModel.onKakaoLogin() }
+    )
+}
+
+@Composable
+fun LoginContent(
+    onKakaoLogin: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(OnDotColor.Gray900),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 100.dp)
+                .background(OnDotColor.GradientLogin)
+        )
+
+        Image(
+            painter = painterResource(Res.drawable.ic_login),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(top = 189.dp)
+                .width(240.dp)
+                .height(85.dp)
+        )
+
+        LoginButtons(
+            onKakaoLogin = onKakaoLogin
+        )
+    }
+}
+
+@Composable
+fun LoginButtons(
+    onKakaoLogin: () -> Unit = {},
+    onAppleLogin: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
+
+        OnDotButton(
+            buttonText = KAKAO_LOGIN_BUTTON_TEXT,
+            buttonType = ButtonType.Kakao,
+            onClick = onKakaoLogin
+        )
+
+        if (getPlatform().name == IOS) {
+
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
