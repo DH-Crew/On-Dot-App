15b0565bd7e956451cfd97bfc278abfa7ebc9d5b
dogmania <hj296914@gmail.com>
2025-07-04 03:41:53 +0900

    :sparkles: Feat: 시스템 네비게이션바 padding 적용

--- composeApp/src/androidMain/kotlin/com/dh/ondot/presentation/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/dh/ondot/presentation/MainActivity.kt
@@ -1,11 +1,19 @@
-package com.dh.ondot
+package com.dh.ondot.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.dh.ondot.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +21,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            Box(
+                modifier = Modifier
+                    .windowInsetsPadding(
+                        WindowInsets.navigationBars.only(WindowInsetsSides.Bottom)
+                    )
+            ) {
+                App()
+            }
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
