836fb898eedf2252cecbdad7a7aa3f0ccbdc8eea
dogmania <hj296914@gmail.com>
2025-07-04 03:46:26 +0900

    :sparkles: Feat: 프로젝트 공통 버튼 구현

---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/ui/components/OnDotButton.kt
@@ -0,0 +1,60 @@
+package com.dh.ondot.presentation.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.dh.ondot.core.ui.extensions.styles
+import com.dh.ondot.domain.model.enums.ButtonType
+import com.dh.ondot.domain.model.enums.OnDotTextStyle
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun OnDotButton(
+    buttonText: String,
+    buttonType: ButtonType,
+    onClick: () -> Unit = {}
+) {
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 22.dp)
+            .fillMaxWidth()
+            .height(60.dp)
+            .background(color = buttonType.styles().backgroundColor, shape = RoundedCornerShape(12.dp))
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (buttonType == ButtonType.Kakao) {
+                buttonType.styles().drawableResource?.let {
+                    Image(
+                        painter = painterResource(it),
+                        contentDescription = null,
+                        modifier = Modifier.width(21.dp).height(20.dp)
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            OnDotText(
+                text = buttonText,
+                style = OnDotTextStyle.TitleSmallSB
+            )
+        }
+    }
+}
+
