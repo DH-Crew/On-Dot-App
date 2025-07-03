27c50b848f89807d368d23f5d95f909304681796
dogmania <hj296914@gmail.com>
2025-07-04 03:42:48 +0900

    :sparkles: Feat: 안드로이드 카카오 로그인 설정

--- composeApp/src/androidMain/kotlin/com/dh/ondot/app/OnDotApplication.kt
+++ composeApp/src/androidMain/kotlin/com/dh/ondot/app/OnDotApplication.kt
@@ -1,11 +1,14 @@
 package com.dh.ondot.app
 
 import android.app.Application
+import com.dh.ondot.BuildConfig
 import com.dh.ondot.util.AppContextHolder
+import com.kakao.sdk.common.KakaoSdk
 
 class OnDotApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         AppContextHolder.init(this)
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
     }
 }
