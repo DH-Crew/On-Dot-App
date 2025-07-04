4dbbe3c5dfe19f3eb61167f18af873a83542c08d
dogmania <hj296914@gmail.com>
2025-07-05 02:32:34 +0900

    :sparkles: Feat: LoginViewModel 구현

---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/login/LoginViewModel.kt
@@ -0,0 +1,37 @@
+package com.dh.ondot.presentation.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import co.touchlab.kermit.Logger
+import com.dh.ondot.core.di.ServiceLocator
+import com.dh.ondot.core.di.kakaoSignIn
+import com.dh.ondot.data.model.TokenModel
+import com.dh.ondot.domain.repository.AuthRepository
+import kotlinx.coroutines.launch
+
+class LoginViewModel(
+    private val authRepository: AuthRepository = ServiceLocator.authRepository
+): ViewModel() {
+    private val logger = Logger.withTag("LoginViewModel")
+
+    fun onKakaoLogin() {
+        kakaoSignIn { token ->
+            viewModelScope.launch {
+                authRepository.login("KAKAO", token).collect { result ->
+                    result.onSuccess { authResponse ->
+                        saveToken(token = TokenModel(accessToken = authResponse.accessToken, refreshToken = authResponse.refreshToken))
+                    }
+                    result.onFailure { throwable ->
+                        logger.d { "throwable: $throwable" }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun saveToken(token: TokenModel) {
+        viewModelScope.launch {
+            authRepository.saveToken(token)
+        }
+    }
+}
