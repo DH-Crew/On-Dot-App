d369eed6280631d7c1d51b954a1b77f68a19b5e9
dogmania <hj296914@gmail.com>
2025-07-05 02:30:57 +0900

    :sparkles: Feat: 플랫폼에 따른 httpClient expect/actual 구현

---
+++ composeApp/src/androidMain/kotlin/com/dh/ondot/core/di/HttpClient.kt
@@ -0,0 +1,32 @@
+package com.dh.ondot.core.di
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+actual fun httpClient(): HttpClient = HttpClient(OkHttp) {
+    install(ContentNegotiation) {
+        json(
+            Json {
+                ignoreUnknownKeys = true
+                isLenient       = true
+            }
+        )
+    }
+
+    install(Logging) {
+        logger = object : Logger {
+            override fun log(message: String) = println("Ktor ▶ $message")
+        }
+        level = LogLevel.BODY
+    }
+
+    defaultRequest { header("Content-Type", "application/json") }
+}
---
+++ composeApp/src/iosMain/kotlin/com/dh/ondot/core/di/HttpClient.kt
@@ -0,0 +1,33 @@
+package com.dh.ondot.core.di
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.darwin.Darwin
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import platform.Foundation.NSLog
+
+actual fun httpClient(): HttpClient = HttpClient(Darwin) {
+    install(ContentNegotiation) {
+        json(
+            Json {
+                ignoreUnknownKeys = true
+                isLenient       = true
+            }
+        )
+    }
+
+    install(Logging) {
+        logger = object : Logger {
+            override fun log(message: String) = NSLog("Ktor ▶ %@", message)
+        }
+        level = LogLevel.ALL
+    }
+
+    defaultRequest { header("Content-Type", "application/json") }
+}
