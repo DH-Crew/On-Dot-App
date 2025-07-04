a222c51340c91956d1b7635d14d1e157c06dafc3
dogmania <hj296914@gmail.com>
2025-07-05 02:56:16 +0900

    :sparkles: Feat: 앱 전체 NavHost 구현

--- composeApp/src/commonMain/kotlin/com/dh/ondot/App.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/App.kt
@@ -1,35 +1,27 @@
 package com.dh.ondot
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.dh.ondot.presentation.login.LoginScreen
-import com.dh.ondot.presentation.splash.SplashScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.dh.ondot.core.navigation.NavRoutes
+import com.dh.ondot.core.navigation.loginNavGraph
+import com.dh.ondot.core.navigation.splashNavGraph
 import com.dh.ondot.presentation.ui.theme.OnDotTheme
-import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun App() {
+    val navController = rememberNavController()
+
     OnDotTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
+        NavHost(
+            navController = navController,
+            startDestination = NavRoutes.SplashGraph.route,
+            modifier = Modifier.fillMaxSize()
         ) {
-            LoginScreen()
+            splashNavGraph(navController = navController)
+            loginNavGraph(navController = navController)
         }
     }
 }
