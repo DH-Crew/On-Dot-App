1512b87ce7ed2b2d330813062281f2cd1b1f8caa
dogmania <hj296914@gmail.com>
2025-07-04 18:14:57 +0900

    :sparkles: Feat: ServiceLocator 구현

---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/core/di/ServiceLocator.kt
@@ -0,0 +1,20 @@
+package com.dh.ondot.core.di
+
+import com.dh.ondot.data.repository.AuthRepositoryImpl
+import com.dh.ondot.domain.repository.AuthRepository
+import com.dh.ondot.network.NetworkClient
+import com.dh.ondot.network.TokenProvider
+
+object ServiceLocator {
+    private lateinit var tokenProvider: TokenProvider
+    private lateinit var networkClient: NetworkClient
+
+    val authRepository: AuthRepository by lazy {
+        AuthRepositoryImpl(networkClient)
+    }
+
+    fun init(tokenProvider: TokenProvider) {
+        this.tokenProvider = tokenProvider
+        this.networkClient = NetworkClient(tokenProvider)
+    }
+}
