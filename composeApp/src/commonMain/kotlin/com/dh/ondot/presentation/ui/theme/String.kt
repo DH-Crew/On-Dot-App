ee94d8d2e35733d0b6176b1d311f45acae04e35b
dogmania <hj296914@gmail.com>
2025-07-06 00:15:27 +0900

    :bento: Chore: 리소스 추가

--- composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/ui/theme/String.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/ui/theme/String.kt
@@ -4,5 +4,16 @@ package com.dh.ondot.presentation.ui.theme
 const val ANDROID = "Android"
 const val IOS = "IOS"
 
+// 단어
+const val WORD_NEXT = "다음"
+const val WORD_HOUR = "시간"
+const val WORD_MINUTE = "분"
+
 // 버튼 텍스트
 const val KAKAO_LOGIN_BUTTON_TEXT = "카카오로 계속하기"
+
+// Onboarding
+const val ONBOARDING1_TITLE = "평소 외출 준비하는데\n얼마나 소요되나요?"
+const val ONBOARDING1_TITLE_HIGHLIGHT = "외출 준비"
+const val ONBOARDING1_SUB_TITLE = "30분, 1시간 20분 등 자유롭게 적어주세요!"
+const val ONBOARDING1_PLACEHOLDER = "5자 이내의 숫자"
