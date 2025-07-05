8a6f8c2654b8de7edaa0ed2e9b36980557261a98
dogmania <hj296914@gmail.com>
2025-07-06 04:51:05 +0900

    :sparkles: Feat: PlaceRepository 구현

---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/data/repository/PlaceRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.dh.ondot.data.repository
+
+import com.dh.ondot.domain.model.response.AddressInfo
+import com.dh.ondot.domain.repository.PlaceRepository
+import com.dh.ondot.network.HttpMethod
+import com.dh.ondot.network.NetworkClient
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class PlaceRepositoryImpl(
+    private val networkClient: NetworkClient
+): PlaceRepository {
+    override suspend fun searchPlace(query: String): Flow<Result<List<AddressInfo>>> = flow {
+        val response = networkClient.request<List<AddressInfo>>(
+            method = HttpMethod.GET,
+            path = "/places/search",
+            queryParams = mapOf("query" to query)
+        )
+
+        emit(response.fold(
+            onSuccess = { Result.success(it) },
+            onFailure = { Result.failure(it) }
+        ))
+    }
+}
