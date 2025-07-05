a49931218604bc5d6d057b0cfb49f01a9a376eff
dogmania <hj296914@gmail.com>
2025-07-06 04:54:18 +0900

    :bento: Chore: 리소스 추가

--- composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/ui/theme/String.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/ui/theme/String.kt
@@ -17,3 +17,8 @@ const val ONBOARDING1_TITLE = "평소 외출 준비하는데\n얼마나 소요�
 const val ONBOARDING1_TITLE_HIGHLIGHT = "외출 준비"
 const val ONBOARDING1_SUB_TITLE = "30분, 1시간 20분 등 자유롭게 적어주세요!"
 const val ONBOARDING1_PLACEHOLDER = "5자 이내의 숫자"
+
+const val ONBOARDING2_TITLE = "주소를 입력해 주세요."
+const val ONBOARDING2_TITLE_HIGHLIGHT = "주소"
+const val ONBOARDING2_SUB_TITLE = "빠른 일정 등록에 꼭 필요한 정보예요. \n집 주소는 안전하게 저장되며 언제든 수정할 수 있어요."
+const val ONBOARDING2_PLACEHOLDER = "도로명 주소"
