e7f177d09aff034abb2e4110fafb202ffb18c14b
dogmania <hj296914@gmail.com>
2025-07-07 00:49:55 +0900

    :sparkles: Feat: Playback 적용

--- composeApp/src/iosMain/kotlin/com/dh/ondot/core/util/IosSoundPlayer.kt
+++ composeApp/src/iosMain/kotlin/com/dh/ondot/core/util/IosSoundPlayer.kt
@@ -3,6 +3,9 @@ package com.dh.ondot.core.util
 import com.dh.ondot.domain.di.SoundPlayer
 import kotlinx.cinterop.ExperimentalForeignApi
 import platform.AVFAudio.AVAudioPlayer
+import platform.AVFAudio.AVAudioSession
+import platform.AVFAudio.AVAudioSessionCategoryPlayback
+import platform.AVFAudio.setActive
 import platform.Foundation.NSBundle
 import platform.Foundation.NSURL
 
@@ -10,19 +13,29 @@ import platform.Foundation.NSURL
 class IosSoundPlayer: SoundPlayer {
     private var player: AVAudioPlayer? = null
 
+    init {
+        AVAudioSession.sharedInstance().setCategory(
+            category = AVAudioSessionCategoryPlayback,
+            error = null
+        )
+        AVAudioSession.sharedInstance().setActive(true, error = null)
+    }
+
     override fun playSound(soundResId: String) {
         stopSound()
 
-        val url: NSURL? = NSBundle.mainBundle.URLForResource(
+        val url: NSURL = NSBundle.mainBundle.URLForResource(
             name = soundResId,
             withExtension = "mp3"
-        )
-        if (url != null) {
-            player = AVAudioPlayer(contentsOfURL = url, error = null).also {
-                it.numberOfLoops = 0
-                it.prepareToPlay()
-                it.play()
-            }
+        ) ?: run {
+            println("resource를 찾을 수 없습니다.")
+            return
+        }
+
+        player = AVAudioPlayer(contentsOfURL = url, error = null).also {
+            it.numberOfLoops = 0
+            it.prepareToPlay()
+            it.play()
         }
     }
 
