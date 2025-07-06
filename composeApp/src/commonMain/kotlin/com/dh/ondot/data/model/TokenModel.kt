cf67e6ad0547b7b3ed85061582bc76da35d750ad
dogmania <hj296914@gmail.com>
2025-07-07 00:49:39 +0900

    :sparkles: Feat: Serializable 적용

--- composeApp/src/commonMain/kotlin/com/dh/ondot/data/model/TokenModel.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/data/model/TokenModel.kt
@@ -1,5 +1,8 @@
 package com.dh.ondot.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class TokenModel(
     val accessToken: String = "",
     val refreshToken: String = ""
