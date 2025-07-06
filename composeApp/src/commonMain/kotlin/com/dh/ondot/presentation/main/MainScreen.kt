d2ebc7000aac55bc5c7e89276d1e2ea752f50985
dogmania <hj296914@gmail.com>
2025-07-07 04:25:45 +0900

    :sparkles: Feat: BottomNavBar 상단 border 적용

--- composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/main/MainScreen.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,6 +25,10 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.dh.ondot.domain.model.enums.BottomNavType
+import com.dh.ondot.domain.model.enums.OnDotTextStyle
+import com.dh.ondot.presentation.ui.components.OnDotText
+import com.dh.ondot.presentation.ui.theme.OnDotColor
+import com.dh.ondot.presentation.ui.theme.OnDotColor.Gray500
 import com.dh.ondot.presentation.ui.theme.OnDotColor.Gray800
 import ondot.composeapp.generated.resources.Res
 import ondot.composeapp.generated.resources.ic_home_selected
@@ -48,6 +55,13 @@ fun MainScreen(
         }
     ) {
         // TODO(HomeScreen, SettingScreen을 분기처리하는 로직 구현)
+
+        Box(
+            modifier = Modifier.fillMaxSize().background(color = OnDotColor.Gray900),
+            contentAlignment = Alignment.Center
+        ) {
+            OnDotText("임시 화면", style = OnDotTextStyle.BodyLargeSB, color = OnDotColor.Gray0)
+        }
     }
 }
 
@@ -58,27 +72,31 @@ fun BottomNavBar(
     interactionSource: MutableInteractionSource,
     onClick: (String) -> Unit = {},
 ) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .height(72.dp)
-            .background(Gray800.copy(alpha = 0.8f)),
-        horizontalArrangement = Arrangement.Absolute.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        BottomNavItem(
-            resourceId = if (type == BottomNavType.HOME) Res.drawable.ic_home_selected else Res.drawable.ic_home_unselected,
-            type = BottomNavType.HOME,
-            interactionSource = interactionSource,
-            onClick = onClick
-        )
+    Column {
+        HorizontalDivider(color = Gray500, thickness = 1.dp)
 
-        BottomNavItem(
-            resourceId = if (type == BottomNavType.SETTING) Res.drawable.ic_settings_selected else Res.drawable.ic_settings_unselected,
-            type = BottomNavType.SETTING,
-            interactionSource = interactionSource,
-            onClick = onClick
-        )
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(72.dp)
+                .background(Gray800.copy(alpha = 0.8f)),
+            horizontalArrangement = Arrangement.Absolute.SpaceEvenly,
+            verticalAlignment = Alignment.Top
+        ) {
+            BottomNavItem(
+                resourceId = if (type == BottomNavType.HOME) Res.drawable.ic_home_selected else Res.drawable.ic_home_unselected,
+                type = BottomNavType.HOME,
+                interactionSource = interactionSource,
+                onClick = onClick
+            )
+
+            BottomNavItem(
+                resourceId = if (type == BottomNavType.SETTING) Res.drawable.ic_settings_selected else Res.drawable.ic_settings_unselected,
+                type = BottomNavType.SETTING,
+                interactionSource = interactionSource,
+                onClick = onClick
+            )
+        }
     }
 }
 
