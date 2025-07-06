2aafdd4e61ec354fc4d28542f8d77ee60fb76359
dogmania <hj296914@gmail.com>
2025-07-07 03:16:53 +0900

    :sparkles: Feat: 커스텀 ToastHost 구현

---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/core/ui/util/ToastHost.kt
@@ -0,0 +1,117 @@
+package com.dh.ondot.core.ui.util
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.dh.ondot.domain.model.enums.OnDotTextStyle
+import com.dh.ondot.domain.model.enums.ToastType
+import com.dh.ondot.domain.model.ui.ToastData
+import com.dh.ondot.presentation.ui.components.OnDotText
+import com.dh.ondot.presentation.ui.theme.OnDotColor
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
+import ondot.composeapp.generated.resources.Res
+import ondot.composeapp.generated.resources.ic_circle_check_green
+import ondot.composeapp.generated.resources.ic_circle_check_red
+import ondot.composeapp.generated.resources.ic_circle_error_red
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun ToastHost(
+    modifier: Modifier = Modifier
+) {
+    val toasts = ToastManager.toasts
+    var current by remember { mutableStateOf<ToastData?>(null) }
+
+    LaunchedEffect(Unit) {
+        toasts
+            .collectLatest { toast ->
+                current = toast
+                delay(toast.duration)
+                current = null
+            }
+    }
+
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        current?.let { toast ->
+            AnimatedVisibility(
+                visible = true,
+                enter = slideInVertically { -it } + fadeIn(),
+                exit = slideOutVertically { -it } + fadeOut()
+            ) {
+                ToastItem(
+                    data = toast,
+                    onDismiss = { current = null }
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+private fun ToastItem(
+    data: ToastData,
+    onDismiss: () -> Unit
+) {
+    val bg = OnDotColor.Gray500
+    val icon = when(data.type) {
+        ToastType.INFO    -> Res.drawable.ic_circle_check_green
+        ToastType.DELETE -> Res.drawable.ic_circle_check_red
+        ToastType.ERROR   -> Res.drawable.ic_circle_error_red
+    }
+    val textColor = when(data.type) {
+        ToastType.INFO    -> OnDotColor.Green700
+        ToastType.DELETE, ToastType.ERROR -> OnDotColor.Red
+    }
+
+    Box(modifier = Modifier
+        .padding(top = 50.dp)
+        .padding(horizontal = 22.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(bg, RoundedCornerShape(12.dp))
+                .padding(horizontal = 16.dp, vertical = 12.dp)
+                .clickable { onDismiss() }
+        ) {
+            Image(
+                painter = painterResource(icon),
+                contentDescription = null,
+                modifier = Modifier.size(16.dp)
+            )
+
+            Spacer(Modifier.width(8.dp))
+
+            OnDotText(data.message, color = textColor, style = OnDotTextStyle.BodyLargeSB)
+        }
+    }
+}
