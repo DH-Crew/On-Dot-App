9d05ff8cefa7fe9d8cf26280f19e0f7563b2ba94
dogmania <hj296914@gmail.com>
2025-07-07 00:48:59 +0900

    :sparkles: Feat: 온보딩 완료 API 서버 통신 구현

---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/domain/model/request/QuestionAnswer.kt
@@ -0,0 +1,9 @@
+package com.dh.ondot.domain.model.request
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class QuestionAnswer(
+    val questionId: Long,
+    val answerId: Long
+)
