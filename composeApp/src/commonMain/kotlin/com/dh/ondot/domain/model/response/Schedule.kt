6115bbe07f2ee355f21cbdb0006d6cb3ce2f0c93
dogmania <hj296914@gmail.com>
2025-07-07 23:55:53 +0900

    :sparkles: Feat: 전체 일정 조회 응답 데이터 클래스 구현

---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/domain/model/response/Schedule.kt
@@ -0,0 +1,20 @@
+package com.dh.ondot.domain.model.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Schedule(
+    val scheduleId: Long,
+    val startLongitude: Double,
+    val startLatitude: Double,
+    val endLongitude: Double,
+    val endLatitude: Double,
+    val scheduleTitle: String,
+    val isRepeat: Boolean,
+    val repeatDays: List<Int>,
+    val appointmentAt: String,
+    val preparationAlarm: AlarmDetail,
+    val departureAlarm: AlarmDetail,
+    val nextAlarmAt: String,
+    val isEnabled: Boolean
+)
---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/domain/model/response/ScheduleListResponse.kt
@@ -0,0 +1,10 @@
+package com.dh.ondot.domain.model.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ScheduleListResponse(
+    val earliestAlarmAt: String,
+    val hasNext: Boolean,
+    val scheduleList: List<Schedule>
+)
