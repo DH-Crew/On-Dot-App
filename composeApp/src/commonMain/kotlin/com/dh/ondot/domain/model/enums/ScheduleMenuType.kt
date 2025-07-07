3df563b0f1df1fe3acaf04144e971dc3b9c176af
dogmania <hj296914@gmail.com>
2025-07-07 23:39:08 +0900

    :sparkles: Feat: AddScheduleButton Composable 구현

---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/domain/model/enums/ScheduleMenuType.kt
@@ -0,0 +1,8 @@
+package com.dh.ondot.domain.model.enums
+
+import com.dh.ondot.presentation.ui.theme.GENERAL_ADD
+import com.dh.ondot.presentation.ui.theme.QUICK_ADD
+
+enum class ScheduleMenuType(val text: String) {
+    Quick(QUICK_ADD), General(GENERAL_ADD)
+}
