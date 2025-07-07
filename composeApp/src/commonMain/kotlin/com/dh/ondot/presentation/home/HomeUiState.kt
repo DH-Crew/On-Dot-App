5f20029bd6b269143d0c21d1946f96b19f3d8057
dogmania <hj296914@gmail.com>
2025-07-08 02:06:54 +0900

    :sparkles: Feat: HomeUiState 구현

---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/home/HomeUiState.kt
@@ -0,0 +1,10 @@
+package com.dh.ondot.presentation.home
+
+import com.dh.ondot.core.ui.base.UiState
+import com.dh.ondot.domain.model.response.Schedule
+
+data class HomeUiState(
+    val remainingTime: Triple<Int, Int, Int> = Triple(0, 30, 0),
+    val isExpanded: Boolean = false,
+    val scheduleList: List<Schedule> = emptyList()
+): UiState
