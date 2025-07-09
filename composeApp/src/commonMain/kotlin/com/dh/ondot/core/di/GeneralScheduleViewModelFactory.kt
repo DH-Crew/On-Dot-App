aa1c765a4513b54f3f951f4d3d08e966db9df76c
dogmania <hj296914@gmail.com>
2025-07-09 20:53:23 +0900

    :recycle: Refactor: GeneralScheduleViewModelFactory 타입 안정성 추가

--- composeApp/src/commonMain/kotlin/com/dh/ondot/core/di/GeneralScheduleViewModelFactory.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/core/di/GeneralScheduleViewModelFactory.kt
@@ -3,9 +3,21 @@ package com.dh.ondot.core.di
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import com.dh.ondot.domain.repository.ScheduleRepository
 import com.dh.ondot.presentation.general.GeneralScheduleViewModel
 import kotlin.reflect.KClass
 
-class GeneralScheduleViewModelFactory(): ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: KClass<T>, extras: CreationExtras): T = GeneralScheduleViewModel() as T
+class GeneralScheduleViewModelFactory(
+    private val scheduleRepository: ScheduleRepository = ServiceLocator.scheduleRepository
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(
+        modelClass: KClass<T>,
+        extras: CreationExtras
+    ): T {
+        if (modelClass == GeneralScheduleViewModel::class) {
+            @Suppress("Unchecked_cast")
+            return GeneralScheduleViewModel(scheduleRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.simpleName}")
+    }
 }
