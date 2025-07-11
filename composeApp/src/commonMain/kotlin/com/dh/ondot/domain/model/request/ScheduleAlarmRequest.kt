b7df722d9904e2a9375910b2c32dba1c3eb077d1
dogmania <hj296914@gmail.com>
2025-07-11 16:55:53 +0900

    :sparkles: Feat: 경로 기반 알람 생성 요청, 응답 데이터 클래스 정의

---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/domain/model/request/ScheduleAlarmRequest.kt
@@ -0,0 +1,9 @@
+package com.dh.ondot.domain.model.request
+
+data class ScheduleAlarmRequest(
+    val appointmentAt: String,
+    val startLongitude: Double,
+    val startLatitude: Double,
+    val endLongitude: Double,
+    val endLatitude: Double
+)
