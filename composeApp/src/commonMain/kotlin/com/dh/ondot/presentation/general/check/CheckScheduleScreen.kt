5deae89a9977a798dd48991ee64151ed7e18815b
dogmania <hj296914@gmail.com>
2025-07-11 15:10:55 +0900

    :sparkles: Feat: Composable 생성

---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/general/check/CheckScheduleScreen.kt
@@ -0,0 +1,17 @@
+package com.dh.ondot.presentation.general.check
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CheckScheduleScreen(
+
+) {
+
+}
+
+@Composable
+fun CheckScheduleContent(
+
+) {
+    
+}
