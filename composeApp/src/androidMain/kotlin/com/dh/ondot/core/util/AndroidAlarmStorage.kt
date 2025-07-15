c3aa194cd7bb1bbdccc39c8acde87082aaed8a11
dogmania <hj296914@gmail.com>
2025-07-16 00:35:06 +0900

    :sparkles: Feat: DataStore를 활용해 알람 정보를 저장하는 로직 구현

---
+++ composeApp/src/androidMain/kotlin/com/dh/ondot/core/util/AndroidAlarmStorage.kt
@@ -0,0 +1,35 @@
+package com.dh.ondot.core.util
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.dh.ondot.data.dataStore
+import com.dh.ondot.domain.di.AlarmStorage
+import com.dh.ondot.domain.model.response.AlarmDetail
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class AndroidAlarmStorage(
+    private val context: Context
+) : AlarmStorage {
+
+    private val dataStore = context.dataStore
+
+    private val ALARMS_KEY = stringPreferencesKey("alarm_list")
+
+    override val alarmsFlow: Flow<List<AlarmDetail>> = dataStore.data
+        .map { prefs ->
+            prefs[ALARMS_KEY]?.let { json ->
+                Json.decodeFromString<List<AlarmDetail>>(json)
+            } ?: emptyList()
+        }
+
+    override suspend fun saveAlarms(alarms: List<AlarmDetail>) {
+        val json = Json.encodeToString(alarms)
+        dataStore.edit { prefs ->
+            prefs[ALARMS_KEY] = json
+        }
+    }
+}
