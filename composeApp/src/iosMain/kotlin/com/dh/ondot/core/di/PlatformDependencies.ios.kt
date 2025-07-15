c9e33624e3ad1850f640d3022f277be3bb4c9f1e
dogmania <hj296914@gmail.com>
2025-07-16 00:29:53 +0900

    :sparkles: Feat: AlarmStorage, AlarmScheduler에 대한 expect/actual 구현

--- composeApp/src/iosMain/kotlin/com/dh/ondot/core/di/PlatformDependencies.ios.kt
+++ composeApp/src/iosMain/kotlin/com/dh/ondot/core/di/PlatformDependencies.ios.kt
@@ -2,12 +2,22 @@ package com.dh.ondot.core.di
 
 import androidx.compose.runtime.Composable
 import com.dh.ondot.core.util.IosSoundPlayer
+import com.dh.ondot.domain.di.AlarmScheduler
+import com.dh.ondot.domain.di.AlarmStorage
 import com.dh.ondot.domain.di.SoundPlayer
 import com.dh.ondot.network.TokenProvider
 
 actual fun provideTokenProvider(): TokenProvider = TokenProvider()
 
 actual fun provideSoundPlayer(): SoundPlayer = IosSoundPlayer()
+
+actual fun provideAlarmStorage(): AlarmStorage {
+    TODO("Not yet implemented")
+}
+
+actual fun provideAlarmScheduler(): AlarmScheduler {
+    TODO("Not yet implemented")
+}
 
 @Composable
 actual fun BackPressHandler(onBack: () -> Unit) {} // Ios에서는 무시
