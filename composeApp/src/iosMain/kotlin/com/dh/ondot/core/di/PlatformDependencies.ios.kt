ec44554fb49d7e2b80d738ceb8159abe7e7e0a6d
dogmania <hj296914@gmail.com>
2025-07-18 02:23:33 +0900

    :sparkles: Feat: AlarmScheduler, AlarmStorage 의존성 주입 로직 구현

--- composeApp/src/iosMain/kotlin/com/dh/ondot/core/di/PlatformDependencies.ios.kt
+++ composeApp/src/iosMain/kotlin/com/dh/ondot/core/di/PlatformDependencies.ios.kt
@@ -1,6 +1,8 @@
 package com.dh.ondot.core.di
 
 import androidx.compose.runtime.Composable
+import com.dh.ondot.core.util.IosAlarmScheduler
+import com.dh.ondot.core.util.IosAlarmStorage
 import com.dh.ondot.core.util.IosSoundPlayer
 import com.dh.ondot.domain.di.AlarmScheduler
 import com.dh.ondot.domain.di.AlarmStorage
@@ -11,13 +13,9 @@ actual fun provideTokenProvider(): TokenProvider = TokenProvider()
 
 actual fun provideSoundPlayer(): SoundPlayer = IosSoundPlayer()
 
-actual fun provideAlarmStorage(): AlarmStorage {
-    TODO("Not yet implemented")
-}
+actual fun provideAlarmStorage(): AlarmStorage = IosAlarmStorage()
 
-actual fun provideAlarmScheduler(): AlarmScheduler {
-    TODO("Not yet implemented")
-}
+actual fun provideAlarmScheduler(): AlarmScheduler = IosAlarmScheduler()
 
 @Composable
 actual fun BackPressHandler(onBack: () -> Unit) {} // Ios에서는 무시
