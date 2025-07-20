cc9c305e32b1f882855bbc3d32bb1c9575ebeb63
dogmania <hj296914@gmail.com>
2025-07-20 22:43:38 +0900

    :sparkles: Feat: 알람 이벤트 발생 시 알람 정보에 따라 알람 재생 화면을 렌더링

--- composeApp/src/commonMain/kotlin/com/dh/ondot/App.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/App.kt
@@ -8,10 +8,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.dh.ondot.core.navigation.NavRoutes
+import com.dh.ondot.core.navigation.alarmNavGraph
 import com.dh.ondot.core.navigation.generalScheduleNavGraph
 import com.dh.ondot.core.navigation.loginNavGraph
 import com.dh.ondot.core.navigation.mainNavGraph
@@ -21,6 +25,7 @@ import com.dh.ondot.core.ui.util.DismissKeyboardOnClick
 import com.dh.ondot.core.ui.util.ToastHost
 import com.dh.ondot.core.util.AlarmNotifier
 import com.dh.ondot.domain.model.ui.AlarmEvent
+import com.dh.ondot.presentation.app.AppViewModel
 import com.dh.ondot.presentation.ui.theme.OnDotTheme
 
 @Composable
@@ -28,11 +33,13 @@ fun App(
     initialAlarm: AlarmEvent? = null
 ) {
     val navController = rememberNavController()
+    val viewModel: AppViewModel = viewModel { AppViewModel() }
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     // android 에서 MainActivity 로부터 전달된 이벤트 처리
     LaunchedEffect(initialAlarm) {
-        initialAlarm?.let { (type, alarmId) ->
-            TODO("네비게이션 로직 구현")
+        initialAlarm?.let { (alarmId, type) ->
+            navController.navigate(NavRoutes.PreparationAlarm.createRoute(alarmId))
         }
     }
 
@@ -77,6 +84,7 @@ fun App(
                     },
                     modifier = Modifier.fillMaxSize()
                 ) {
+                    alarmNavGraph(navController)
                     splashNavGraph(navController = navController)
                     loginNavGraph(navController = navController)
                     onboardingNavGraph(navController = navController)
--- composeApp/src/commonMain/kotlin/com/dh/ondot/core/navigation/NavGraph.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/core/navigation/NavGraph.kt
@@ -4,9 +4,13 @@ import androidx.compose.runtime.remember
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navigation
+import androidx.navigation.toRoute
 import com.dh.ondot.core.di.GeneralScheduleViewModelFactory
+import com.dh.ondot.presentation.alarm.preparation.PreparationAlarmRingScreen
 import com.dh.ondot.presentation.general.GeneralScheduleViewModel
 import com.dh.ondot.presentation.general.check.CheckScheduleScreen
 import com.dh.ondot.presentation.general.loading.RouteLoadingScreen
@@ -16,6 +20,33 @@ import com.dh.ondot.presentation.login.LoginScreen
 import com.dh.ondot.presentation.main.MainScreen
 import com.dh.ondot.presentation.onboarding.OnboardingScreen
 import com.dh.ondot.presentation.splash.SplashScreen
+
+fun NavGraphBuilder.alarmNavGraph(
+    navController: NavHostController
+) {
+    navigation(
+        startDestination = NavRoutes.PreparationAlarm.ROUTE,
+        route = NavRoutes.AlarmGraph.route
+    ) {
+        composable(
+            NavRoutes.PreparationAlarm.ROUTE,
+            arguments = listOf(navArgument("alarmId") { type = NavType.LongType })
+        ) { backStackEntry ->
+            val args = backStackEntry.toRoute<NavRoutes.PreparationAlarm>()
+            val alarmId = args.alarmId
+
+            PreparationAlarmRingScreen(
+                alarmId = alarmId,
+                navigateToSplash = {
+                    navController.navigate(NavRoutes.Splash.route) {
+                        popUpTo(NavRoutes.AlarmGraph.route) { inclusive = true }
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+    }
+}
 
 fun NavGraphBuilder.splashNavGraph(navController: NavHostController) {
     navigation(
