99ba36f639a27dca881bd5c475bdd72455dfda69
dogmania <hj296914@gmail.com>
2025-07-20 22:40:14 +0900

    :sparkles: Feat: SoundPlayer 의존성 관리 로직 구현

--- composeApp/src/commonMain/kotlin/com/dh/ondot/core/di/ServiceLocator.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/core/di/ServiceLocator.kt
@@ -4,12 +4,13 @@ import com.dh.ondot.data.repository.AuthRepositoryImpl
 import com.dh.ondot.data.repository.MemberRepositoryImpl
 import com.dh.ondot.data.repository.PlaceRepositoryImpl
 import com.dh.ondot.data.repository.ScheduleRepositoryImpl
-import com.dh.ondot.domain.service.AlarmScheduler
-import com.dh.ondot.domain.service.AlarmStorage
 import com.dh.ondot.domain.repository.AuthRepository
 import com.dh.ondot.domain.repository.MemberRepository
 import com.dh.ondot.domain.repository.PlaceRepository
 import com.dh.ondot.domain.repository.ScheduleRepository
+import com.dh.ondot.domain.service.AlarmScheduler
+import com.dh.ondot.domain.service.AlarmStorage
+import com.dh.ondot.domain.service.SoundPlayer
 import com.dh.ondot.network.NetworkClient
 import com.dh.ondot.network.TokenProvider
 
@@ -18,6 +19,7 @@ object ServiceLocator {
     private lateinit var networkClient: NetworkClient
     private lateinit var alarmStorage: AlarmStorage
     private lateinit var alarmScheduler: AlarmScheduler
+    private lateinit var soundPlayer: SoundPlayer
 
     val authRepository: AuthRepository by lazy {
         AuthRepositoryImpl(networkClient, tokenProvider)
@@ -38,16 +40,19 @@ object ServiceLocator {
     fun init(
         tokenProvider: TokenProvider,
         alarmStorage: AlarmStorage,
-        alarmScheduler: AlarmScheduler
+        alarmScheduler: AlarmScheduler,
+        soundPlayer: SoundPlayer
     ) {
         this.tokenProvider = tokenProvider
         this.networkClient = NetworkClient(tokenProvider)
         this.alarmStorage = alarmStorage
         this.alarmScheduler = alarmScheduler
+        this.soundPlayer = soundPlayer
     }
 
     fun provideNetworkClient(): NetworkClient = networkClient
     fun provideTokenProvider(): TokenProvider = tokenProvider
     fun provideAlarmStorage(): AlarmStorage = alarmStorage
     fun provideAlarmScheduler(): AlarmScheduler = alarmScheduler
+    fun provideSoundPlayer(): SoundPlayer = soundPlayer
 }
