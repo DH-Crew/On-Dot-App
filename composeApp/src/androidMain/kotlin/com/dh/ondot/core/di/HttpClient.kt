226290b166d2984ab8870b70f2ab1ed599fff5a1
dogmania <hj296914@gmail.com>
2025-07-23 21:17:55 +0900

    :recycle: Refactor: 로깅 방법 수정

--- composeApp/src/androidMain/kotlin/com/dh/ondot/core/di/HttpClient.kt
+++ composeApp/src/androidMain/kotlin/com/dh/ondot/core/di/HttpClient.kt
@@ -8,10 +8,17 @@ import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
+import io.ktor.client.plugins.observer.ResponseObserver
 import io.ktor.client.request.header
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
+@OptIn(ExperimentalSerializationApi::class)
 actual fun httpClient(): HttpClient = HttpClient(OkHttp) {
     install(ContentNegotiation) {
         json(
@@ -23,11 +30,33 @@ actual fun httpClient(): HttpClient = HttpClient(OkHttp) {
         )
     }
 
+    val prettyJson = Json {
+        prettyPrint        = true
+        prettyPrintIndent  = "  "
+        ignoreUnknownKeys  = true
+        isLenient          = true
+        encodeDefaults     = true
+    }
+
     install(Logging) {
-        logger = object : Logger {
-            override fun log(message: String) = println("Ktor ▶ $message")
+        logger = Logger.SIMPLE
+        level  = if (BuildConfig.DEBUG) LogLevel.HEADERS else LogLevel.NONE
+    }
+
+    install(ResponseObserver) {
+        onResponse { response ->
+            if (response.status.value in 200..299 &&
+                response.headers[HttpHeaders.ContentType]?.startsWith("application/json") == true) {
+                val text = response.bodyAsText()
+
+                try {
+                    val element = prettyJson.parseToJsonElement(text)
+                    println("Ktor ▶\n${prettyJson.encodeToString(element)}")
+                } catch (_: Exception) {
+                    println("Ktor ▶\n$text")
+                }
+            }
         }
-        level = if (BuildConfig.DEBUG) LogLevel.BODY else LogLevel.NONE
     }
 
     defaultRequest { header("Content-Type", "application/json") }
