998a153ee308bd383698c6ac8e2ebdfe036f79f7
dogmania <hj296914@gmail.com>
2025-07-23 21:18:25 +0900

    :recycle: Refactor: 로컬 저장소에 저장할 데이터 클래스 변경

--- composeApp/src/iosMain/kotlin/com/dh/ondot/core/util/IosAlarmStorage.kt
+++ composeApp/src/iosMain/kotlin/com/dh/ondot/core/util/IosAlarmStorage.kt
@@ -1,7 +1,7 @@
 package com.dh.ondot.core.util
 
+import com.dh.ondot.domain.model.ui.AlarmRingInfo
 import com.dh.ondot.domain.service.AlarmStorage
-import com.dh.ondot.domain.model.response.AlarmDetail
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -15,19 +15,19 @@ class IosAlarmStorage: AlarmStorage {
 
     private val defaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
     private val _alarmsFlow = MutableStateFlow(getAlarms())
-    override val alarmsFlow: Flow<List<AlarmDetail>> = _alarmsFlow.asStateFlow()
+    override val alarmsFlow: Flow<List<AlarmRingInfo>> = _alarmsFlow.asStateFlow()
 
-    override suspend fun saveAlarms(alarms: List<AlarmDetail>) {
+    override suspend fun saveAlarms(alarms: List<AlarmRingInfo>) {
         // JSON 으로 직렬화하여 UserDefaults 에 저장
         val json = Json.encodeToString(alarms)
         defaults.setObject(json, ALARMS_KEY)
         defaults.synchronize()
 
-        // 2) Flow 에 새 값 방출
+        // Flow 에 새 값 방출
         _alarmsFlow.value = alarms
     }
 
-    private fun getAlarms(): List<AlarmDetail> {
+    private fun getAlarms(): List<AlarmRingInfo> {
         // UserDefaults 에서 꺼낸 문자열이 없으면 빈 리스트
         val json = defaults.stringForKey(ALARMS_KEY) ?: return emptyList()
         return try {
