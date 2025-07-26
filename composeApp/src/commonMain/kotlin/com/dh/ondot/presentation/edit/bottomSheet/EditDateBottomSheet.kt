bb1147dc4a8f0973a9164a99c5185ebabbfe5612
dogmania <hj296914@gmail.com>
2025-07-27 00:34:31 +0900

    :recycle: Refactor: 패키지명 변경

--- composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/edit/bottomSheet/EditDateBottomSheet.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/edit/bottomSheet/EditDateBottomSheet.kt
@@ -1,10 +1,13 @@
 package com.dh.ondot.presentation.edit.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import kotlinx.datetime.LocalDate
 
 @Composable
@@ -14,7 +17,14 @@ fun EditDateBottomSheet(
     currentDate: LocalDate,
     onEditDate: (Boolean, Set<Int>, LocalDate) -> Unit,
 ) {
-    var isRepeatState by remember { mutableStateOf(isRepeat) }
-    var repeatDaysState by remember { mutableStateOf(repeatDays) }
-    var currentDateState by remember { mutableStateOf(currentDate) }
+    var isRepeatState by remember(isRepeat) { mutableStateOf(isRepeat) }
+    var repeatDaysState by remember(repeatDays) { mutableStateOf(repeatDays) }
+    var currentDateState by remember(currentDate) { mutableStateOf(currentDate) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        
+    }
 }
