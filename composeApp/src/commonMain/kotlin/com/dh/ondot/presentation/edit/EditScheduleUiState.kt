48c30255bdfa9207a4fb11e10329422dd1f425f6
dogmania <hj296914@gmail.com>
2025-07-27 02:35:09 +0900

    :sparkles: Feat: 날짜 변경 상태 변수 추가

--- composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/edit/EditScheduleUiState.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/edit/EditScheduleUiState.kt
@@ -2,10 +2,22 @@ package com.dh.ondot.presentation.edit
 
 import com.dh.ondot.core.ui.base.UiState
 import com.dh.ondot.domain.model.response.ScheduleDetail
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 data class EditScheduleUiState(
     val scheduleId: Long = -1,
     val isInitialized: Boolean = false,
     val schedule: ScheduleDetail = ScheduleDetail(),
-    val showDeleteDialog: Boolean = false
+    val selectedDate: LocalDate? = null,
+    val selectedTime: LocalTime =
+        Clock.System.now()
+            .toLocalDateTime(TimeZone.currentSystemDefault())
+            .time,
+    val showDeleteDialog: Boolean = false,
+    val showDateBottomSheet: Boolean = false,
+    val showTimeBottomSheet: Boolean = false
 ) : UiState
