2ed885dc4859d00f1a69001e8392b02c48f91e65
dogmania <hj296914@gmail.com>
2025-07-26 15:32:01 +0900

    :sparkles: Feat: 일정 상세 조회 서버 통신 구현

--- composeApp/src/commonMain/kotlin/com/dh/ondot/domain/repository/ScheduleRepository.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/domain/repository/ScheduleRepository.kt
@@ -3,6 +3,7 @@ package com.dh.ondot.domain.repository
 import com.dh.ondot.domain.model.request.CreateScheduleRequest
 import com.dh.ondot.domain.model.request.ScheduleAlarmRequest
 import com.dh.ondot.domain.model.response.ScheduleAlarmResponse
+import com.dh.ondot.domain.model.response.ScheduleDetailResponse
 import com.dh.ondot.domain.model.response.ScheduleListResponse
 import kotlinx.coroutines.flow.Flow
 
@@ -10,4 +11,5 @@ interface ScheduleRepository {
     suspend fun getScheduleList(): Flow<Result<ScheduleListResponse>>
     suspend fun getScheduleAlarms(request: ScheduleAlarmRequest): Flow<Result<ScheduleAlarmResponse>>
     suspend fun createSchedule(request: CreateScheduleRequest): Flow<Result<Unit>>
+    suspend fun getScheduleDetail(scheduleId: Long): Flow<Result<ScheduleDetailResponse>>
 }
