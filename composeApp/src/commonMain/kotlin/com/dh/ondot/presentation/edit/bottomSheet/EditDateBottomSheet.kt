de015dbf86b11828662867cef8dade40b7fd3be1
dogmania <hj296914@gmail.com>
2025-07-27 02:36:01 +0900

    :sparkles: Feat: 날짜 편집 바텀시트 구현

--- composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/edit/bottomSheet/EditDateBottomSheet.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/edit/bottomSheet/EditDateBottomSheet.kt
@@ -1,13 +1,28 @@
-package com.dh.ondot.presentation.edit.components
+package com.dh.ondot.presentation.edit.bottomSheet
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.dh.ondot.domain.model.enums.ButtonType
+import com.dh.ondot.presentation.general.repeat.components.Calendar
+import com.dh.ondot.presentation.general.repeat.components.DateSectionHeader
+import com.dh.ondot.presentation.general.repeat.components.RepeatSettingSection
+import com.dh.ondot.presentation.ui.components.OnDotBottomSheet
+import com.dh.ondot.presentation.ui.components.OnDotButton
+import com.dh.ondot.presentation.ui.theme.OnDotColor.Gray600
+import com.dh.ondot.presentation.ui.theme.WORD_COMPETE
 import kotlinx.datetime.LocalDate
 
 @Composable
@@ -15,16 +30,80 @@ fun EditDateBottomSheet(
     isRepeat: Boolean,
     repeatDays: Set<Int>,
     currentDate: LocalDate,
-    onEditDate: (Boolean, Set<Int>, LocalDate) -> Unit,
+    onEditDate: (Boolean, Set<Int>, LocalDate?) -> Unit,
+    onDismiss: () -> Unit
 ) {
-    var isRepeatState by remember(isRepeat) { mutableStateOf(isRepeat) }
-    var repeatDaysState by remember(repeatDays) { mutableStateOf(repeatDays) }
-    var currentDateState by remember(currentDate) { mutableStateOf(currentDate) }
+    val viewModel: EditBottomSheetViewModel = viewModel { EditBottomSheetViewModel() }
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val interactionSource = remember { MutableInteractionSource() }
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
-        
+    LaunchedEffect(Unit) {
+        viewModel.initDate(isRepeat, repeatDays, currentDate)
     }
+
+    DisposableEffect(Unit) {
+        onDispose { viewModel.clear() }
+    }
+
+    OnDotBottomSheet(
+        onDismiss = onDismiss,
+        contentPaddingTop = 16.dp,
+        contentPaddingBottom = 16.dp,
+        sheetMaxHeightFraction = 0.8f,
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                RepeatSettingSection(
+                    isRepeat = uiState.isRepeat,
+                    activeCheckChip = uiState.activeCheckChip,
+                    activeWeekDays = uiState.repeatDays,
+                    onClickSwitch = viewModel::onClickSwitch,
+                    onClickTextChip = viewModel::onClickTextChip,
+                    onClickCheckTextChip = viewModel::onClickCheckTextChip
+                )
+
+                HorizontalDivider(thickness = (0.5).dp, color = Gray600, modifier = Modifier.fillMaxWidth())
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                DateSectionHeader(
+                    selectedDate = uiState.currentDate,
+                    interactionSource = interactionSource,
+                    isActiveCalendar = true,
+                    isRepeat = uiState.isRepeat,
+                    activeWeekDays = uiState.repeatDays,
+                    onToggleCalendar = {  }
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                HorizontalDivider(thickness = (0.5).dp, color = Gray600, modifier = Modifier.fillMaxWidth())
+
+                Calendar(
+                    month = uiState.calendarMonth,
+                    selectedDate = uiState.currentDate,
+                    isRepeat = uiState.isRepeat,
+                    activeWeekDays = uiState.repeatDays,
+                    onPrevMonth = viewModel::onPrevMonth,
+                    onNextMonth = viewModel::onNextMonth,
+                    onDateSelected = viewModel::onDateSelected
+                )
+
+                Spacer(modifier = Modifier.height(26.dp))
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                OnDotButton(
+                    buttonText = WORD_COMPETE,
+                    buttonType = ButtonType.Green500,
+                    onClick = {
+                        onEditDate(uiState.isRepeat, uiState.repeatDays, uiState.currentDate)
+                        onDismiss()
+                    }
+                )
+            }
+        }
+    )
 }
