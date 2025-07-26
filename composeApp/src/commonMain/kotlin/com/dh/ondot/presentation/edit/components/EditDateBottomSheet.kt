0be611fbe31e3bcbc7ca8dcebb5e50cbac5100af
dogmania <hj296914@gmail.com>
2025-07-26 23:13:18 +0900

    :sparkles: Feat: 파일 생성

---
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/edit/components/EditDateBottomSheet.kt
@@ -0,0 +1,20 @@
+package com.dh.ondot.presentation.edit.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import kotlinx.datetime.LocalDate
+
+@Composable
+fun EditDateBottomSheet(
+    isRepeat: Boolean,
+    repeatDays: Set<Int>,
+    currentDate: LocalDate,
+    onEditDate: (Boolean, Set<Int>, LocalDate) -> Unit,
+) {
+    var isRepeatState by remember { mutableStateOf(isRepeat) }
+    var repeatDaysState by remember { mutableStateOf(repeatDays) }
+    var currentDateState by remember { mutableStateOf(currentDate) }
+}
