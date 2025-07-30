66c7f70d8fa4d8e2863d635bbf29cd37a7c09e0a
dogmania <hj296914@gmail.com>
2025-07-31 00:14:25 +0900

    :sparkles: Feat: 출발 알람 미루기 화면 구현

--- composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/alarm/departure/DepartureAlarmRingScreen.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/alarm/departure/DepartureAlarmRingScreen.kt
@@ -11,12 +11,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -25,16 +23,20 @@ import com.dh.ondot.domain.model.enums.ButtonType
 import com.dh.ondot.domain.model.enums.OnDotTextStyle
 import com.dh.ondot.domain.model.response.AlarmDetail
 import com.dh.ondot.getPlatform
-import com.dh.ondot.presentation.alarm.preparation.AlarmSnoozedSection
 import com.dh.ondot.presentation.alarm.preparation.ScheduleInfoSection
 import com.dh.ondot.presentation.app.AppViewModel
 import com.dh.ondot.presentation.ui.components.OnDotButton
+import com.dh.ondot.presentation.ui.components.OnDotHighlightText
 import com.dh.ondot.presentation.ui.components.OnDotText
 import com.dh.ondot.presentation.ui.theme.ANDROID
 import com.dh.ondot.presentation.ui.theme.DEPARTURE_ALARM_RING_TITLE
 import com.dh.ondot.presentation.ui.theme.OnDotColor.Gray0
 import com.dh.ondot.presentation.ui.theme.OnDotColor.Gray900
-import com.dh.ondot.presentation.ui.theme.PREPARATION_START_BUTTON_TEXT
+import com.dh.ondot.presentation.ui.theme.OnDotColor.Green500
+import com.dh.ondot.presentation.ui.theme.OnDotColor.Red
+import com.dh.ondot.presentation.ui.theme.SHOW_ROUTE_INFORMATION_BUTTON_TEXT
+import com.dh.ondot.presentation.ui.theme.TEMP_DEPARTURE_SNOOZED_TITLE
+import com.dh.ondot.presentation.ui.theme.formatRemainingSnoozeTime
 import io.github.alexzhirkevich.compottie.Compottie
 import io.github.alexzhirkevich.compottie.LottieCompositionSpec
 import io.github.alexzhirkevich.compottie.animateLottieCompositionAsState
@@ -57,20 +59,18 @@ fun DepartureAlarmRingScreen(
         }
     }
 
-    LaunchedEffect(uiState.showDepartureSnoozeAnimation) {
-        if (uiState.showDepartureSnoozeAnimation) {
-            delay(2000L)
-            navigateToSplash()
-        }
+    if (uiState.alarmRingInfo.appointmentAt.isNotBlank()) {
+        DepartureAlarmRingContent(
+            alarmDetail = uiState.alarmRingInfo.alarmDetail,
+            appointmentAt = uiState.alarmRingInfo.appointmentAt,
+            scheduleTitle = uiState.alarmRingInfo.scheduleTitle,
+            showDepartureSnoozeAnimation = uiState.showDepartureSnoozeAnimation,
+            onSnoozeDepartureAlarm = viewModel::snoozeDepartureAlarm,
+            onShowRouteInfo = {}
+        )
+    } else {
+        Box(modifier = Modifier.fillMaxSize().background(Gray900))
     }
-
-    DepartureAlarmRingContent(
-        alarmDetail = uiState.alarmRingInfo.alarmDetail,
-        appointmentAt = uiState.alarmRingInfo.appointmentAt,
-        scheduleTitle = uiState.alarmRingInfo.scheduleTitle,
-        showDepartureSnoozeAnimation = uiState.showDepartureSnoozeAnimation,
-        onSnoozeDepartureAlarm = viewModel::snoozeDepartureAlarm
-    )
 }
 
 @Composable
@@ -79,13 +79,13 @@ fun DepartureAlarmRingContent(
     appointmentAt: String,
     scheduleTitle: String,
     showDepartureSnoozeAnimation: Boolean,
-    onSnoozeDepartureAlarm: () -> Unit
+    onSnoozeDepartureAlarm: () -> Unit,
+    onShowRouteInfo: () -> Unit
 ) {
     val appointmentDate = DateTimeFormatter.formatKoreanDate(appointmentAt)
     val appointmentTime = DateTimeFormatter.formatHourMinute(appointmentAt)
-    var isSnoozed by remember { mutableStateOf(false) }
     val composition by rememberLottieComposition {
-        LottieCompositionSpec.JsonString(Res.readBytes("files/lotties/preparation_alarm_snoozed.json").decodeToString())
+        LottieCompositionSpec.JsonString(Res.readBytes("files/lotties/departure_alarm_snoozed.json").decodeToString())
     }
     val progress by animateLottieCompositionAsState(
         composition,
@@ -95,22 +95,6 @@ fun DepartureAlarmRingContent(
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        if (showDepartureSnoozeAnimation) {
-            Box(
-                modifier = Modifier.fillMaxSize().background(Gray900)
-            ) {
-                Image(
-                    painter = rememberLottiePainter(
-                        composition = composition,
-                        progress = { progress }
-                    ),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.FillWidth
-                )
-            }
-        }
-
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -124,28 +108,101 @@ fun DepartureAlarmRingContent(
             OnDotText(
                 text = DEPARTURE_ALARM_RING_TITLE,
                 style = OnDotTextStyle.TitleMediumSB,
-                color = Gray0
+                color = Gray0,
+                textAlign = TextAlign.Center
             )
 
-            if (isSnoozed) {
-                AlarmSnoozedSection(alarmDetail.snoozeInterval)
-            } else {
-                ScheduleInfoSection(
-                    snoozeInterval = alarmDetail.snoozeInterval,
-                    appointmentDate = appointmentDate,
-                    appointmentTime = appointmentTime,
-                    scheduleTitle = scheduleTitle,
-                    onClickSnooze = { isSnoozed = true }
-                )
-            }
+            ScheduleInfoSection(
+                snoozeInterval = alarmDetail.snoozeInterval,
+                appointmentDate = appointmentDate,
+                appointmentTime = appointmentTime,
+                scheduleTitle = scheduleTitle,
+                onClickSnooze = onSnoozeDepartureAlarm
+            )
 
             Spacer(modifier = Modifier.weight(1f))
 
             OnDotButton(
-                buttonText = PREPARATION_START_BUTTON_TEXT,
+                buttonText = SHOW_ROUTE_INFORMATION_BUTTON_TEXT,
                 buttonType = ButtonType.Gradient,
                 onClick = onSnoozeDepartureAlarm
             )
         }
+
+        if (showDepartureSnoozeAnimation) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Gray900),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = rememberLottiePainter(
+                        composition = composition,
+                        progress = { progress }
+                    ),
+                    contentDescription = null,
+                    modifier = Modifier.matchParentSize(),
+                )
+
+                AlarmSnoozedSection(alarmDetail.snoozeInterval, onShowRouteInfo = onShowRouteInfo)
+            }
+        }
+    }
+}
+
+@Composable
+private fun AlarmSnoozedSection(
+    snoozeInterval: Int,
+    onShowRouteInfo: () -> Unit
+) {
+    val totalSeconds = snoozeInterval * 60
+    val timeLeftSecond by produceState(initialValue = totalSeconds) {
+        var current = totalSeconds
+
+        while (current > 0) {
+            delay(1000)
+            current--
+            value = current
+        }
+    }
+    val minutes = timeLeftSecond / 60
+    val seconds = timeLeftSecond % 60
+    val timeText = formatRemainingSnoozeTime(minutes, seconds)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 22.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(69.dp))
+
+        OnDotHighlightText(
+            text = TEMP_DEPARTURE_SNOOZED_TITLE,
+            textColor = Gray0,
+            highlight = "NN:NN",
+            highlightColor = Green500,
+            style = OnDotTextStyle.TitleMediumSB,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(79.dp))
+
+        OnDotText(
+            text = timeText,
+            style = OnDotTextStyle.TitleLargeM,
+            color = Red
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        OnDotButton(
+            buttonText = SHOW_ROUTE_INFORMATION_BUTTON_TEXT,
+            buttonType = ButtonType.Gradient,
+            onClick = onShowRouteInfo
+        )
+
+        Spacer(modifier = if (getPlatform().name == ANDROID) Modifier.height(16.dp) else Modifier.height(37.dp))
     }
 }
