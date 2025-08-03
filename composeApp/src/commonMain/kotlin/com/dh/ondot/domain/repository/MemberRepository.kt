d43ddee280f3121a6c9e92c9558e3598f4b235b3
dogmania <hj296914@gmail.com>
2025-08-03 22:44:26 +0900

    :recycle: Refactor: 회원탈퇴 요청 Dto 추가

--- composeApp/src/commonMain/kotlin/com/dh/ondot/domain/repository/MemberRepository.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/domain/repository/MemberRepository.kt
@@ -1,6 +1,7 @@
 package com.dh.ondot.domain.repository
 
 import com.dh.ondot.data.model.TokenModel
+import com.dh.ondot.domain.model.request.DeleteAccountRequest
 import com.dh.ondot.domain.model.request.OnboardingRequest
 import com.dh.ondot.domain.model.response.HomeAddressInfo
 import kotlinx.coroutines.flow.Flow
@@ -8,5 +9,5 @@ import kotlinx.coroutines.flow.Flow
 interface MemberRepository {
     suspend fun completeOnboarding(request: OnboardingRequest): Flow<Result<TokenModel>>
     suspend fun getHomeAddress(): Flow<Result<HomeAddressInfo>>
-    suspend fun withdrawUser(): Flow<Result<Unit>>
+    suspend fun withdrawUser(request: DeleteAccountRequest): Flow<Result<Unit>>
 }
