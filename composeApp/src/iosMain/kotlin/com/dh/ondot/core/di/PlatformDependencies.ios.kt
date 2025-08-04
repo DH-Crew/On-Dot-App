86350a57b7c47a831c89f3a9c4ac778a75b6e9d4
dogmania <hj296914@gmail.com>
2025-08-04 23:23:06 +0900

    :sparkles: Feat: WebView expect/actual 구현

--- composeApp/src/iosMain/kotlin/com/dh/ondot/core/di/PlatformDependencies.ios.kt
+++ composeApp/src/iosMain/kotlin/com/dh/ondot/core/di/PlatformDependencies.ios.kt
@@ -1,6 +1,10 @@
 package com.dh.ondot.core.di
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.UIKitView
 import com.dh.ondot.core.util.IosAlarmScheduler
 import com.dh.ondot.core.util.IosAlarmStorage
 import com.dh.ondot.core.util.IosSoundPlayer
@@ -8,6 +12,9 @@ import com.dh.ondot.domain.service.AlarmScheduler
 import com.dh.ondot.domain.service.AlarmStorage
 import com.dh.ondot.domain.service.SoundPlayer
 import com.dh.ondot.network.TokenProvider
+import platform.Foundation.NSMutableURLRequest
+import platform.Foundation.NSURL
+import platform.WebKit.WKWebView
 
 actual fun provideTokenProvider(): TokenProvider = TokenProvider()
 
@@ -19,3 +26,19 @@ actual fun provideAlarmScheduler(): AlarmScheduler = IosAlarmScheduler()
 
 @Composable
 actual fun BackPressHandler(onBack: () -> Unit) {} // Ios에서는 무시
+
+@Composable
+actual fun WebView(url: String, modifier: Modifier) {
+    val webView = remember { WKWebView() }
+    val request = NSMutableURLRequest.requestWithURL(URL = NSURL(string = url))
+
+    webView.apply {
+        loadRequest(request)
+        allowsBackForwardNavigationGestures = true // Swipe Back 제스쳐
+    }
+
+    UIKitView(
+        factory = { webView },
+        modifier = Modifier.fillMaxSize().then(modifier)
+    )
+}
