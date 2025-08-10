89da98e24952785643eb23fcf35e10c27e6aac08
dogmania <hj296914@gmail.com>
2025-08-10 19:30:04 +0900

    :sparkles: Feat: MapProviderStorage 초기화 로직 구현

--- composeApp/src/androidMain/kotlin/com/dh/ondot/app/OnDotApplication.kt
+++ composeApp/src/androidMain/kotlin/com/dh/ondot/app/OnDotApplication.kt
@@ -2,12 +2,8 @@ package com.dh.ondot.app
 
 import android.app.Application
 import com.dh.ondot.BuildConfig
-import com.dh.ondot.core.di.ServiceLocator
-import com.dh.ondot.core.di.provideAlarmScheduler
-import com.dh.ondot.core.di.provideAlarmStorage
-import com.dh.ondot.core.di.provideSoundPlayer
-import com.dh.ondot.core.di.provideTokenProvider
 import com.dh.ondot.core.initKoin
+import com.dh.ondot.core.initShared
 import com.dh.ondot.util.AppContextHolder
 import com.kakao.sdk.common.KakaoSdk
 
@@ -16,12 +12,7 @@ class OnDotApplication: Application() {
         super.onCreate()
         AppContextHolder.init(this)
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
-        ServiceLocator.init(
-            provideTokenProvider(),
-            provideAlarmStorage(),
-            provideAlarmScheduler(),
-            provideSoundPlayer()
-        )
+        initShared()
         initKoin()
     }
 }
