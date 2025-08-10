cfca01f38f911ad33f5457c85c7585e5ddfaa281
dogmania <hj296914@gmail.com>
2025-08-10 18:46:35 +0900

    :recycle: Refactor: 알람 로직 안정성 개선

--- composeApp/src/commonMain/kotlin/com/dh/ondot/core/util/AlarmNotifier.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/core/util/AlarmNotifier.kt
@@ -1,14 +1,16 @@
 package com.dh.ondot.core.util
 
 import com.dh.ondot.domain.model.ui.AlarmEvent
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 
 object AlarmNotifier {
     private val _events = MutableSharedFlow<AlarmEvent>(
-        replay = 1, // 늦게 구독한 쪽에도 전달, ios에서 화면 전환이 안돼서 추가한 코드
-        extraBufferCapacity = 1
+        replay = 0,
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
     )
     val events: SharedFlow<AlarmEvent> = _events.asSharedFlow()
 
