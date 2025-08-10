c4009b873b90b7a365ea1a59ed486c2da405b51d
dogmania <hj296914@gmail.com>
2025-08-10 19:19:47 +0900

    :sparkles: Feat: ios 지도 공급자 로컬 저장소 구현

---
+++ composeApp/src/iosMain/kotlin/com/dh/ondot/core/util/IosMapProviderStorage.kt
@@ -0,0 +1,40 @@
+package com.dh.ondot.core.util
+
+import com.dh.ondot.domain.model.enums.MapProvider
+import com.dh.ondot.domain.service.MapProviderStorage
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import platform.Foundation.NSNotificationCenter
+import platform.Foundation.NSOperationQueue
+import platform.Foundation.NSUserDefaults
+import platform.Foundation.NSUserDefaultsDidChangeNotification
+
+class IosMapProviderStorage: MapProviderStorage {
+    private val defaults = NSUserDefaults.standardUserDefaults
+    private val key = "map_provider"
+
+    private val state = MutableStateFlow(load())
+
+    init {
+        NSNotificationCenter.defaultCenter.addObserverForName(
+            name = NSUserDefaultsDidChangeNotification,
+            `object` = defaults,
+            queue = NSOperationQueue.mainQueue
+        ) { _ ->
+            state.value = load()
+        }
+    }
+
+    override fun getMapProvider(): Flow<MapProvider> = state
+
+    override suspend fun setMapProvider(mapProvider: MapProvider) {
+        defaults.setObject(mapProvider.name, forKey = key)
+        defaults.synchronize() // 즉시 디스크 반영
+        state.value = mapProvider
+    }
+
+    private fun load(): MapProvider =
+        runCatching {
+            defaults.stringForKey(key)?.let { enumValueOf<MapProvider>(it) }
+        }.getOrNull() ?: MapProvider.KAKAO
+}
