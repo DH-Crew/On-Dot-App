1e5f5e13f7edfbe3d23b48b2da141507f0fff27b
dogmania <hj296914@gmail.com>
2025-08-10 16:11:54 +0900

    :recycle: Refactor: ios 알람 정보 파싱 로직 수정 및 알람 화면 렌더링

--- composeApp/src/commonMain/kotlin/com/dh/ondot/core/util/AlarmNotifier.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/core/util/AlarmNotifier.kt
@@ -6,10 +6,15 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 
 object AlarmNotifier {
-    private val _events = MutableSharedFlow<AlarmEvent>(extraBufferCapacity = 1)
+    private val _events = MutableSharedFlow<AlarmEvent>(
+        replay = 1, // 늦게 구독한 쪽에도 전달, ios에서 화면 전환이 안돼서 추가한 코드
+        extraBufferCapacity = 1
+    )
     val events: SharedFlow<AlarmEvent> = _events.asSharedFlow()
 
     fun notify(event: AlarmEvent) {
-        _events.tryEmit(event)
+        println("[AlarmNotifier] tryEmit $event")
+        val ok = _events.tryEmit(event)
+        println("[AlarmNotifier] emitted=$ok replay=${(events as? MutableSharedFlow<AlarmEvent>)?.replayCache?.size}")
     }
 }
