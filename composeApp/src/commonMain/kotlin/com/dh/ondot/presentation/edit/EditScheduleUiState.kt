e7db9c0bf93b68ec8105cbc2d25a7f4377fcca29
dogmania <hj296914@gmail.com>
2025-08-14 23:11:27 +0900

    :recycle: Refactor: 두개의 바텀시트 플래그 통합

--- composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/edit/EditScheduleUiState.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/edit/EditScheduleUiState.kt
@@ -1,6 +1,7 @@
 package com.dh.ondot.presentation.edit
 
 import com.dh.ondot.core.ui.base.UiState
+import com.dh.ondot.domain.model.enums.TimeBottomSheet
 import com.dh.ondot.domain.model.enums.TimeType
 import com.dh.ondot.domain.model.response.ScheduleDetail
 import kotlinx.datetime.Clock
@@ -21,7 +22,6 @@ data class EditScheduleUiState(
     val selectedAlarmDate: LocalDate? = null,
     val showDeleteDialog: Boolean = false,
     val showDateBottomSheet: Boolean = false,
-    val showScheduleTimeBottomSheet: Boolean = false,
-    val showAlarmTimeBottomSheet: Boolean = false,
+    val activeTimeBottomSheet: TimeBottomSheet? = null,
     val selectedTimeType: TimeType = TimeType.DEPARTURE, // 시간을 수정하는 바텀시트가 어떤 것에 의해 렌더링되었는지 저장
 ) : UiState
