25018b3ba4375d17eb9ef24b2843944a09c96214
dogmania <hj296914@gmail.com>
2025-08-14 17:27:43 +0900

    :bento: Chore: String 리소스 추가

--- composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/ui/theme/String.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/ui/theme/String.kt
@@ -134,6 +134,7 @@ fun formatRemainingSnoozeTime(minute: Int, second: Int): String {
 }
 const val DEPARTURE_ALARM_RING_TITLE = "지금 출발해야\n일정에 늦지 않을 수 있어요!"
 fun departureSnoozedTitle(time: String) = "일정까지 $time 전\n어서 출발하세요!"
+const val YESTERDAY_LABEL = "약속 전 날"
 
 // Dialog
 const val DELETE_ALARM_TITLE = "알람 삭제"
