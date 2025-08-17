2a22556017e6ba441e1286251f81fadf0b1abbe8
dogmania <hj296914@gmail.com>
2025-08-17 15:34:54 +0900

    :sparkles: Feat: ios 카카오톡 앱 로그인 설정

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,6 +1,7 @@
 import UIKit
 import SwiftUI
 import ComposeApp
+import KakaoSDKAuth
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
@@ -14,7 +15,14 @@ struct ContentView: View {
     var body: some View {
         ComposeView()
                 .ignoresSafeArea() // Compose has own keyboard handler
+                .onOpenURL { url in
+                    print("Received URL: \(url)")
+                    if AuthApi.isKakaoTalkLoginUrl(url) {
+                        _ = AuthController.handleOpenUrl(url: url, options: [:])
+                    }
+                }
     }
+    
 }
 
 
