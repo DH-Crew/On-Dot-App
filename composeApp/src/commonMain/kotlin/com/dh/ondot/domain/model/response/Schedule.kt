83bcf16c90217e04d1c43885dc9bd9703eda82bc
dogmania <hj296914@gmail.com>
2025-09-07 17:27:36 +0900

    :recycle: Refactor: 기본값 추가

--- composeApp/src/commonMain/kotlin/com/dh/ondot/domain/model/response/Schedule.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/domain/model/response/Schedule.kt
@@ -4,16 +4,16 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Schedule(
-    val scheduleId: Long,
-    val startLongitude: Double,
-    val startLatitude: Double,
-    val endLongitude: Double,
-    val endLatitude: Double,
-    val scheduleTitle: String,
-    val isRepeat: Boolean,
-    val repeatDays: List<Int>,
-    val appointmentAt: String,
-    val preparationAlarm: AlarmDetail,
-    val departureAlarm: AlarmDetail,
-    val hasActiveAlarm: Boolean
+    val scheduleId: Long = -1L,
+    val startLongitude: Double = 0.0,
+    val startLatitude: Double = 0.0,
+    val endLongitude: Double = 0.0,
+    val endLatitude: Double = 0.0,
+    val scheduleTitle: String = "",
+    val isRepeat: Boolean = false,
+    val repeatDays: List<Int> = emptyList(),
+    val appointmentAt: String = "",
+    val preparationAlarm: AlarmDetail = AlarmDetail(),
+    val departureAlarm: AlarmDetail = AlarmDetail(),
+    val hasActiveAlarm: Boolean = false
 )
