4effe2f5050d6cfdca5b26fe390221c9cb2cecc4
dogmania <hj296914@gmail.com>
2025-09-07 17:27:53 +0900

    :recycle: Refactor: scheduleId 추가

--- composeApp/src/commonMain/kotlin/com/dh/ondot/domain/model/ui/AlarmEvent.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/domain/model/ui/AlarmEvent.kt
@@ -3,6 +3,7 @@ package com.dh.ondot.domain.model.ui
 import com.dh.ondot.domain.model.enums.AlarmType
 
 data class AlarmEvent(
+    val scheduleId: Long,
     val alarmId: Long,
     val type: AlarmType
 )
