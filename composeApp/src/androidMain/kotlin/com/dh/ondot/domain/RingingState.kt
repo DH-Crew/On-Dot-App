d8f084076ee9b864d9f521aa654e9c513318037e
dogmania <hj296914@gmail.com>
2025-09-19 01:42:23 +0900

    :sparkles: Feat: RingingState 구현

---
+++ composeApp/src/androidMain/kotlin/com/dh/ondot/domain/RingingState.kt
@@ -0,0 +1,12 @@
+package com.dh.ondot.domain
+
+import com.dh.ondot.domain.model.enums.AlarmType
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RingingState(
+    val isRinging: Boolean = false,
+    val scheduleId: Long = -1,
+    val alarmId: Long = -1,
+    val type: AlarmType = AlarmType.Departure
+)
