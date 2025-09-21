fcc83d0aad6fef4007d7858e415a04163243fb29
dogmania <hj296914@gmail.com>
2025-09-21 17:27:21 +0900

    :sparkles: Feat: Firebase 초기화 로직 추가

--- composeApp/src/androidMain/kotlin/com/dh/ondot/app/OnDotApplication.kt
+++ composeApp/src/androidMain/kotlin/com/dh/ondot/app/OnDotApplication.kt
@@ -6,6 +6,7 @@ import com.dh.ondot.core.di.AndroidServiceLocator
 import com.dh.ondot.core.initKoin
 import com.dh.ondot.core.initShared
 import com.dh.ondot.core.util.AppContextHolder
+import com.google.firebase.FirebaseApp
 import com.kakao.sdk.common.KakaoSdk
 
 class OnDotApplication: Application() {
@@ -13,6 +14,9 @@ class OnDotApplication: Application() {
         super.onCreate()
         AppContextHolder.init(this)
         AndroidServiceLocator.init(context = this)
+        if (FirebaseApp.getApps(this).isEmpty()) {
+            FirebaseApp.initializeApp(this)
+        }
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
         initShared()
         initKoin()
