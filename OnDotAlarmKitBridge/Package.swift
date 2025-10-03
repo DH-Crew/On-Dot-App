9a0fd980f4e70101e98236f148b12b9eebba4c51
dogmania <hj296914@gmail.com>
2025-10-04 00:55:05 +0900

    :sparkles: Feat: linkerOptions에 AlarmKit, UNUserNotifications 추가

--- OnDotAlarmKitBridge/Package.swift
+++ OnDotAlarmKitBridge/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "OnDotAlarmKitBridge",
+    platforms: [.iOS(.v18)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -16,7 +17,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "OnDotAlarmKitBridge"
+            name: "OnDotAlarmKitBridge",
+            dependencies: [],
+            linkerSettings: [
+                .linkedFramework("AlarmKit", .when(platforms: [.iOS])),
+                .linkedFramework("UserNotifications", .when(platforms: [.iOS]))
+            ]
         ),
 
     ]
