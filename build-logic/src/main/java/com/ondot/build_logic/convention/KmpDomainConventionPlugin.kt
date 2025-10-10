0c8d1e889b2b27b44249a74c92e06a1cf1646d6b
dogmania <hj296914@gmail.com>
2025-10-10 17:24:23 +0900

    :sparkles: Feat: Domain 모듈 플러그인 구현

---
+++ build-logic/src/main/java/com/ondot/build_logic/convention/KmpDomainConventionPlugin.kt
@@ -0,0 +1,40 @@
+package com.ondot.build_logic.convention
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.findByType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+
+/**
+ * :domain 모듈에 적용할 convention
+ * */
+class KmpDomainConventionPlugin: Plugin<Project> {
+    override fun apply(target: Project) = with(target) {
+        pluginManager.apply("org.jetbrains.kotlin.multiplatform")
+
+        val libs = project.extensions
+            .findByType<VersionCatalogsExtension>()!!
+            .named("libs")
+
+        extensions.configure<KotlinMultiplatformExtension> {
+            explicitApi()
+            jvm {
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_17)
+                }
+            }
+            iosX64(); iosArm64(); iosSimulatorArm64()
+
+            sourceSets.apply {
+                commonMain {
+                    dependencies {
+                        implementation(libs.findLibrary("kotlinx-coroutines-core").get())
+                    }
+                }
+            }
+        }
+    }
+}
