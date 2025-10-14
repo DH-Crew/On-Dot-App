77ea987f40aa3c87c33b48d5d3d9267e03392034
dogmania <hj296914@gmail.com>
2025-10-15 02:15:13 +0900

    :recycle: Refactor: namespace 추가

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     alias(libs.plugins.androidLibrary)
     id("ondot.kotlin.multiplatform")
 }
+
+kotlin {
+    android {
+        namespace = "com.ondot.core"
+        compileSdk = 36
+    }
+}
