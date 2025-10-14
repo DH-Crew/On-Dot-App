7b29607c318fa086b30bd800b27bbc11b7c2d8f9
dogmania <hj296914@gmail.com>
2025-10-15 02:57:19 +0900

    :sparkles: Feat: KotlinMultiplatformConventionPlugin 구현

--- build-logic/src/main/java/com/ondot/build_logic/convention/KotlinMultiplatformConventionPlugin.kt
+++ build-logic/src/main/java/com/ondot/build_logic/convention/KotlinMultiplatformConventionPlugin.kt
@@ -1,10 +1,15 @@
 package com.ondot.build_logic.convention
 
+import com.ondot.build_logic.convention.internal.configureToolchains
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
 /**
@@ -16,30 +21,59 @@ class KotlinMultiplatformConventionPlugin : Plugin<Project> {
         pluginManager.apply("org.jetbrains.kotlin.multiplatform")
         pluginManager.apply("org.jetbrains.kotlin.plugin.serialization")
 
+        configureToolchains()
+
+        val libs = project.extensions
+            .getByType<VersionCatalogsExtension>()
+            .named("libs")
+
         extensions.configure<KotlinMultiplatformExtension> {
+            applyDefaultHierarchyTemplate()
+            val iosX64T = iosX64()
+            val iosArm64T = iosArm64()
+            val iosSimArm64T = iosSimulatorArm64()
+
             androidTarget {
                 compilerOptions {
                     jvmTarget.set(JvmTarget.JVM_17)
                 }
             }
 
-            val iosArm64Target = iosArm64()
-            val iosSimArm64Target = iosSimulatorArm64()
-
-            // iOS 프레임워크 설정
-            listOf(iosArm64Target, iosSimArm64Target).forEach { target ->
-                target.binaries.framework {
-                    baseName = "ComposeApp"
+            targets.withType<KotlinNativeTarget> {
+                binaries.framework {
+                    baseName = (findProperty("xcframework.basename") as String?) ?: project.name
                     isStatic = true
-                    freeCompilerArgs += listOf(
-                        "-Xbinary=bundleId=com.dh.ondot"
-                    )
+                }
+            }
+
+            sourceSets.apply {
+                commonMain.dependencies {
+                    implementation(libs.findLibrary("kotlinx-coroutines-core").get())
+                    implementation(libs.findLibrary("kotlinx-serialization-json").get())
+                    implementation(libs.findLibrary("ktor-client").get())
+                    implementation(libs.findLibrary("kermit").get())
+                }
+                androidMain.dependencies {
+                    implementation(libs.findLibrary("ktor-client-okhttp").get())
+                }
+                iosMain.dependencies {
+                    implementation(libs.findLibrary("ktor-client-darwin").get())
                 }
             }
 
             val xcFramework = XCFramework("ComposeApp")
-            xcFramework.add(iosArm64Target.binaries.getFramework("DEBUG"))
-            xcFramework.add(iosSimArm64Target.binaries.getFramework("DEBUG"))
+            listOf("Debug","Release").forEach { bt ->
+                listOf(iosArm64T, iosSimArm64T, iosX64T).forEach { t ->
+                    xcFramework.add(t.binaries.getFramework(bt))
+                }
+            }
+
+            tasks.register("packXCFramework") {
+                dependsOn(
+                    "linkDebugFrameworkIosArm64","linkDebugFrameworkIosSimulatorArm64","linkDebugFrameworkIosX64",
+                    "linkReleaseFrameworkIosArm64","linkReleaseFrameworkIosSimulatorArm64","linkReleaseFrameworkIosX64"
+                )
+            }
         }
     }
 }
