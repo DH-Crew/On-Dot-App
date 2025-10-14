295961e477450e62fe6d8c108553f32ac84c2164
dogmania <hj296914@gmail.com>
2025-10-15 02:55:25 +0900

    :sparkles: Feat: AndroidLibraryConventionPlugin 구현

---
+++ build-logic/src/main/java/com/ondot/build_logic/convention/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,19 @@
+package com.ondot.build_logic.convention
+
+import com.android.build.gradle.LibraryExtension
+import com.ondot.build_logic.convention.internal.computeNamespace
+import com.ondot.build_logic.convention.internal.configureAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+
+class AndroidLibraryConventionPlugin: Plugin<Project> {
+    override fun apply(target: Project): Unit = with(target) {
+        pluginManager.apply("com.android.library")
+
+        extensions.getByType<LibraryExtension>().apply {
+            namespace = computeNamespace()
+            configureAndroid(this@with)
+        }
+    }
+}
