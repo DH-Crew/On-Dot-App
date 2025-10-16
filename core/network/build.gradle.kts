78d99965dd17298e971153905f2226b1ac078ec5
dogmania <hj296914@gmail.com>
2025-10-16 19:12:08 +0900

    ✨ Feat: Koin 의존성 추가

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,4 +1,14 @@
 plugins {
     id("convention.android.library")
     id("convention.kmp")
+    id("convention.koin")
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.domain)
+            implementation(projects.core.platform)
+        }
+    }
 }
