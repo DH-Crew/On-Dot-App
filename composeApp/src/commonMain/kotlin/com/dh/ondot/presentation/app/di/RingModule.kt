06e8c4ce2309c51f01986ec157f797ddea69ec31
dogmania <hj296914@gmail.com>
2025-10-19 00:55:37 +0900

    :recycle: Refactor: DirectionsOpener 생성자에서 제거

--- composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/app/di/RingModule.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/app/di/RingModule.kt
@@ -5,5 +5,5 @@ import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val ringModule = module {
-    viewModel<AppViewModel> { AppViewModel(get(), get(), get(), get(), get(), get()) }
+    viewModel<AppViewModel> { AppViewModel(get(), get(), get(), get(), get()) }
 }
