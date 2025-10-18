b50652be1a60b781a8eba00722fb3d702dcbe081
dogmania <hj296914@gmail.com>
2025-10-19 00:56:51 +0900

    :recycle: Refactor: @Serializable 적용

--- domain/src/commonMain/kotlin/com/ondot/domain/model/schedule/ScheduleDetail.kt
+++ domain/src/commonMain/kotlin/com/ondot/domain/model/schedule/ScheduleDetail.kt
@@ -2,7 +2,9 @@ package com.ondot.domain.model.schedule
 
 import com.ondot.domain.model.alarm.Alarm
 import com.ondot.domain.model.member.AddressInfo
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class ScheduleDetail(
     val title: String = "",
     val isRepeat: Boolean = false,
