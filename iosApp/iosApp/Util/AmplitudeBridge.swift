3095434cfff9af3dc817504360ccde6ebbc6b304
dogmania <hj296914@gmail.com>
2025-11-09 22:01:20 +0900

    ✨ Feat: Amplitude Ios Bridge 구현

---
+++ iosApp/iosApp/Util/AmplitudeBridge.swift
@@ -0,0 +1,33 @@
+//
+//  AmplitudeBridge.swift
+//  iosApp
+//
+//  Created by Hyeonsu Son on 11/9/25.
+//
+
+import Foundation
+import AmplitudeSwift
+
+@objc(AmplitudeBridge)
+public final class AmplitudeBridge: NSObject {
+    private var amplitude: Amplitude?
+
+    @objc public static let shared = AmplitudeBridge()
+
+    @objc public func configure(apiKey: String, optOut: Bool = false) {
+        print("API KEY: \(apiKey)")
+        let config = Configuration(apiKey: apiKey)
+        config.optOut = optOut
+        amplitude = Amplitude(configuration: config)
+    }
+
+    @objc public func track(_ name: String, properties: [String: Any]? = nil) {
+        print("TRACK: \(name)")
+        amplitude?.track(eventType: name, eventProperties: properties ?? [:])
+    }
+
+    @objc public func setUserId(_ id: String?) {
+        print("SET USER ID: \(id ?? "nil")")
+        amplitude?.setUserId(userId: id)
+    }
+}
