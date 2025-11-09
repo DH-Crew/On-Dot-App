84c078e102edfb2fe446e03f6cd5f92a587391a2
dogmania <hj296914@gmail.com>
2025-11-09 16:27:02 +0900

    ✨ Feat: Amplitude 의존성 주입

--- composeApp/src/androidMain/kotlin/com/dh/ondot/di/AndroidModule.kt
+++ composeApp/src/androidMain/kotlin/com/dh/ondot/di/AndroidModule.kt
@@ -2,8 +2,24 @@ package com.dh.ondot.di
 
 import android.app.Application
 import android.content.Context
+import com.amplitude.android.Amplitude
+import com.amplitude.android.AutocaptureOption
+import com.amplitude.android.Configuration
+import com.amplitude.android.plugins.SessionReplayPlugin
+import com.dh.ondot.composeApp.BuildKonfig
 import org.koin.dsl.module
 
 fun androidModule(app: Application) = module {
     single<Context> { app.applicationContext }
+    single {
+        Amplitude(
+            Configuration(
+                apiKey = BuildKonfig.AMPLITUDE_KEY,
+                context = get<Context>(),
+                autocapture = AutocaptureOption.ALL
+            )
+        ).apply {
+            add(SessionReplayPlugin())
+        }
+    }
 }
