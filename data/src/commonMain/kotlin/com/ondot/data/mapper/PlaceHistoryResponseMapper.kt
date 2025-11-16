50ba87778b085b6e78f55d28d8befc332464624f
dogmania <hj296914@gmail.com>
2025-11-17 00:34:09 +0900

    ✨ Feat: PlaceHistoryResponse -> PlaceHistory Mapper 구현

---
+++ data/src/commonMain/kotlin/com/ondot/data/mapper/PlaceHistoryResponseMapper.kt
@@ -0,0 +1,20 @@
+package com.ondot.data.mapper
+
+import com.ondot.data.model.response.member.PlaceHistoryResponse
+import com.ondot.domain.model.member.PlaceHistory
+import com.ondot.network.base.Mapper
+
+object PlaceHistoryResponseMapper: Mapper<List<PlaceHistoryResponse>, List<PlaceHistory>> {
+    override fun responseToModel(response: List<PlaceHistoryResponse>?): List<PlaceHistory> {
+        return response?.let { list ->
+            list.map {
+                PlaceHistory(
+                    title = it.title,
+                    latitude = it.latitude,
+                    longitude = it.longitude,
+                    searchedAt = it.searchedAt
+                )
+            }
+        } ?: emptyList()
+    }
+}
