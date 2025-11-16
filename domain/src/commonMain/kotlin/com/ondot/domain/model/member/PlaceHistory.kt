f2c34690914d551427f45c034908aa86a3896ce3
dogmania <hj296914@gmail.com>
2025-11-17 00:34:23 +0900

    ✨ Feat: 검색 기록 비즈니스 모델 구현

---
+++ domain/src/commonMain/kotlin/com/ondot/domain/model/member/PlaceHistory.kt
@@ -0,0 +1,8 @@
+package com.ondot.domain.model.member
+
+data class PlaceHistory(
+    val title: String = "",
+    val longitude: Double = 0.0,
+    val latitude: Double = 0.0,
+    val searchedAt: String = ""
+)
