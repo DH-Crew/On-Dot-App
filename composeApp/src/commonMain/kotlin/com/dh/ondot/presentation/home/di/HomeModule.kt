62d7dcecbd77d1a07ee056c892331eb0c5fded89
dogmania <hj296914@gmail.com>
2025-11-19 00:57:21 +0900

    ♻️ Refactor: viewModelOf 적용

--- composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/home/di/HomeModule.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/home/di/HomeModule.kt
@@ -2,8 +2,9 @@ package com.dh.ondot.presentation.home.di
 
 import com.dh.ondot.presentation.home.HomeViewModel
 import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val homeModule = module {
-    viewModel<HomeViewModel> { HomeViewModel(get(), get(), get(), get()) }
+    viewModelOf((::HomeViewModel))
 }
