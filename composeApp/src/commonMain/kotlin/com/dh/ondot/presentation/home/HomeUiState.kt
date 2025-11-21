72356a2fee69cdbe9a38dc6be92c4e918897f7b1
dogmania <hj296914@gmail.com>
2025-11-21 12:40:55 +0900

    ✨ Feat: 홈화면 일정 제목 옆에 약속시간 추가

--- composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/home/HomeUiState.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/presentation/home/HomeUiState.kt
@@ -5,6 +5,7 @@ import com.dh.ondot.getPlatform
 import com.dh.ondot.presentation.ui.theme.ANDROID
 import com.ondot.domain.model.enums.MapProvider
 import com.ondot.domain.model.schedule.Schedule
+import com.ondot.util.DateTimeFormatter
 
 data class HomeUiState(
     val remainingTime: Triple<Int, Int, Int> = Triple(-1, -1, -1),
@@ -21,4 +22,9 @@ data class HomeUiState(
             MapProvider.NAVER,
             MapProvider.APPLE
         )
-): UiState
+): UiState {
+    companion object {
+        fun appointmentAtTime(date: String) =
+            DateTimeFormatter.formatHourMinute(date)
+    }
+}
