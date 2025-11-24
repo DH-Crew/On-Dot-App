0ff5e851fde04b4f195126b2c57eeea8b205ae42
dogmania <hj296914@gmail.com>
2025-11-24 22:52:32 +0900

    ♻️ Refactor: :feature 모듈 적용

--- composeApp/src/commonMain/kotlin/com/dh/ondot/core/SharedInit.kt
+++ composeApp/src/commonMain/kotlin/com/dh/ondot/core/SharedInit.kt
@@ -1,20 +1,19 @@
 package com.dh.ondot.core
 
 import com.dh.ondot.presentation.app.di.ringModule
-import com.dh.ondot.presentation.edit.di.editScheduleModule
-import com.dh.ondot.presentation.general.di.generalModule
-import com.dh.ondot.presentation.home.di.homeModule
-import com.dh.ondot.presentation.login.di.loginModule
-import com.dh.ondot.presentation.main.di.mainModule
-import com.dh.ondot.presentation.onboarding.di.onboardingModule
-import com.dh.ondot.presentation.setting.di.settingModule
-import com.dh.ondot.presentation.splash.di.splashModule
+import com.ondot.alarm.di.alarmModule
 import com.ondot.data.di.dataSourceModule
 import com.ondot.data.di.databaseModule
 import com.ondot.data.di.provideDriverModule
 import com.ondot.data.di.repositoryModule
+import com.ondot.edit.di.editScheduleModule
+import com.ondot.general.di.generalModule
+import com.ondot.login.di.loginModule
+import com.ondot.main.di.mainModule
 import com.ondot.network.di.networkModule
+import com.ondot.onboarding.di.onboardingModule
 import com.ondot.platform.di.providePlatformModules
+import com.ondot.splash.di.splashModule
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 
@@ -27,16 +26,15 @@ fun initKoin(extraModules: List<Module> = emptyList()) {
                 repositoryModule,
                 networkModule,
                 ringModule,
-                generalModule,
-                homeModule,
-                onboardingModule,
-                splashModule,
-                editScheduleModule,
-                loginModule,
-                mainModule,
-                settingModule,
                 dataSourceModule,
                 databaseModule,
+                splashModule,
+                loginModule,
+                alarmModule,
+                editScheduleModule,
+                generalModule,
+                mainModule,
+                onboardingModule,
                 provideDriverModule()
             )
         )
