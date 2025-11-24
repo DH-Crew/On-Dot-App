7285e9f7c978b690d2e87d34449dc673e73dd965
dogmania <hj296914@gmail.com>
2025-11-24 16:41:44 +0900

    ✨ Feat: SplashNavGraph 구현

--- feature/splash/src/commonMain/kotlin/com/ondot/splash/di/SplashModule.kt
+++ feature/splash/src/commonMain/kotlin/com/ondot/splash/di/SplashModule.kt
@@ -1,9 +1,12 @@
 package com.ondot.splash.di
 
+import com.ondot.navigation.base.NavGraphContributor
 import com.ondot.splash.SplashViewModel
+import com.ondot.splash.navigation.SplashNavGraph
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val splashModule = module {
     viewModelOf(::SplashViewModel)
+    single<NavGraphContributor> { SplashNavGraph }
 }
