2f39bb213c2cc7d763cb1457da79841c08141b70
dogmania <hj296914@gmail.com>
2025-11-24 17:58:25 +0900

    ♻️ Refactor: 공통 컴포넌트 분리

---
+++ core/design-system/src/commonMain/kotlin/com/ondot/design_system/components/PlaceHistoryItem.kt
@@ -0,0 +1,60 @@
+package com.ondot.design_system.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.unit.dp
+import com.dh.ondot.presentation.ui.theme.OnDotColor.Gray300
+import com.dh.ondot.presentation.ui.theme.OnDotColor.Gray400
+import com.dh.ondot.presentation.ui.theme.OnDotColor.Gray50
+import com.ondot.domain.model.enums.OnDotTextStyle
+import com.ondot.domain.model.member.PlaceHistory
+import ondot.core.design_system.generated.resources.Res
+import ondot.core.design_system.generated.resources.ic_close
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun PlaceHistoryItem(
+    item: PlaceHistory,
+    onClick: () -> Unit,
+    formatDate: (String) -> String
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        OnDotText(
+            text = item.title,
+            style = OnDotTextStyle.BodyLargeR1,
+            color = Gray50
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        OnDotText(
+            text = formatDate(item.searchedAt),
+            style = OnDotTextStyle.BodyMediumR,
+            color = Gray300
+        )
+
+        Spacer(Modifier.width(8.dp))
+
+        Image(
+            painter = painterResource(Res.drawable.ic_close),
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(color = Gray400),
+            modifier = Modifier.size(12.dp).clickable{ onClick() }
+        )
+    }
+}
