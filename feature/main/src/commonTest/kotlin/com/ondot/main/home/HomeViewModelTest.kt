909066d6b3bf3057d5575a5076692f633250954b
dogmania <hj296914@gmail.com>
2025-12-03 10:15:05 +0900

    ✨ Feat: 뷰모델에서 사용하는 컴포넌트 객체 생성 및 뷰모델 생성

--- feature/main/src/commonTest/kotlin/com/ondot/main/home/HomeViewModelTest.kt
+++ feature/main/src/commonTest/kotlin/com/ondot/main/home/HomeViewModelTest.kt
@@ -2,8 +2,18 @@ package com.ondot.main.home
 
 import com.ondot.domain.model.alarm.Alarm
 import com.ondot.domain.model.schedule.Schedule
+import com.ondot.domain.repository.MemberRepository
 import com.ondot.domain.repository.ScheduleRepository
+import com.ondot.domain.service.AlarmScheduler
+import com.ondot.domain.service.AnalyticsManager
+import com.ondot.domain.service.DirectionsOpener
+import com.ondot.domain.service.LocalNotificationScheduler
+import com.ondot.testing.fake.FakeMemberRepository
 import com.ondot.testing.fake.FakeScheduleRepository
+import com.ondot.testing.fake.util.FakeAlarmScheduler
+import com.ondot.testing.fake.util.FakeAnalyticsManager
+import com.ondot.testing.fake.util.FakeDirectionsOpener
+import com.ondot.testing.fake.util.FakeNotificationScheduler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
@@ -17,7 +27,12 @@ class HomeViewModelTest {
 
     private val testDispatcher = StandardTestDispatcher()
 
+    private lateinit var memberRepository: MemberRepository
     private lateinit var scheduleRepository: ScheduleRepository
+    private lateinit var directionsOpener: DirectionsOpener
+    private lateinit var alarmScheduler: AlarmScheduler
+    private lateinit var notificationScheduler: LocalNotificationScheduler
+    private lateinit var analyticsManager: AnalyticsManager
 
     private lateinit var viewModel: HomeViewModel
 
@@ -26,6 +41,20 @@ class HomeViewModelTest {
         Dispatchers.setMain(testDispatcher)
 
         scheduleRepository = FakeScheduleRepository()
+        memberRepository = FakeMemberRepository()
+        directionsOpener = FakeDirectionsOpener()
+        alarmScheduler = FakeAlarmScheduler()
+        notificationScheduler = FakeNotificationScheduler()
+        analyticsManager = FakeAnalyticsManager()
+
+        viewModel = HomeViewModel(
+            scheduleRepository = scheduleRepository,
+            memberRepository = memberRepository,
+            directionsOpener = directionsOpener,
+            alarmScheduler = alarmScheduler,
+            notificationScheduler = notificationScheduler,
+            analyticsManager = analyticsManager
+        )
     }
 
     @AfterTest
