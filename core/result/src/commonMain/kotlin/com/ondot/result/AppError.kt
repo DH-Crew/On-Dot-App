c4da6e89ab562a18bd3bd34c2b15e55b2ff5e399
dogmania <hj296914@gmail.com>
2026-02-10 22:50:43 +0900

    ✨ Feat: :core:result 및 AppError, AppResult 구현

---
+++ core/result/src/commonMain/kotlin/com/ondot/result/AppError.kt
@@ -0,0 +1,31 @@
+package com.ondot.result
+
+sealed interface AppError {
+    /** HTTP 4xx/5xx */
+    data class Http(
+        val status: Int, // HTTP status (e.code())
+        val errorCode: String? = null, // 서버에서 반환하는 커스텀 코드 ex) TOKEN_INVALID
+        val message: String? = null, // 서버에서 반환하는 message
+        val rawBody: String? = null, // Http 에러 원문
+    ) : AppError
+
+    /** 네트워크 끊김/불가 */
+    data class Network(
+        val cause: Throwable? = null,
+    ) : AppError
+
+    /** 타임아웃 */
+    data class Timeout(
+        val cause: Throwable? = null,
+    ) : AppError
+
+    /** 파싱/직렬화 */
+    data class Serialization(
+        val cause: Throwable? = null,
+    ) : AppError
+
+    /** 그 외 */
+    data class Unknown(
+        val cause: Throwable,
+    ) : AppError
+}
