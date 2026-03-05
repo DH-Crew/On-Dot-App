12b69a1b0997b2e39979e891cb20abcd5e413c64
dogmania <hj296914@gmail.com>
2026-03-05 10:46:33 +0900

    ✨ Feat: EverytimeNavGraph 정의

---
+++ feature/everytime/src/commonMain/kotlin/com/ondot/everytime/navigation/EverytimeNavGraph.kt
@@ -0,0 +1,29 @@
+package com.ondot.everytime.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.ondot.everytime.LandingRoute
+import com.ondot.navigation.NavRoutes
+import com.ondot.navigation.base.NavGraphContributor
+
+object EverytimeNavGraph: NavGraphContributor {
+    override val graphRoute: NavRoutes
+        get() = NavRoutes.EverytimeGraph
+    override val startDestination: String
+        get() = NavRoutes.Landing.route
+
+    override fun NavGraphBuilder.registerGraph(navController: NavHostController) {
+        navigation(
+            route = graphRoute.route,
+            startDestination = startDestination
+        ) {
+            composable(NavRoutes.Landing.route) {
+                LandingRoute(
+                    popScreen = { navController.popBackStack() }
+                )
+            }
+        }
+    }
+}
