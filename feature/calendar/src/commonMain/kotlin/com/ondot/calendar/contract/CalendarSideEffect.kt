33181cf1e13609b7613f553a0cb95f2247d50796
dogmania <hj296914@gmail.com>
2026-04-03 21:02:36 +0900

    ✨ Feat: 토스트 렌더링 SideEffect 정의

--- feature/calendar/src/commonMain/kotlin/com/ondot/calendar/contract/CalendarSideEffect.kt
+++ feature/calendar/src/commonMain/kotlin/com/ondot/calendar/contract/CalendarSideEffect.kt
@@ -1,5 +1,11 @@
 package com.ondot.calendar.contract
 
+import com.ondot.domain.model.enums.ToastType
 import com.ondot.ui.base.mvi.SideEffect
 
-sealed interface CalendarSideEffect : SideEffect
+sealed interface CalendarSideEffect : SideEffect {
+    data class ShowToast(
+        val message: String,
+        val type: ToastType,
+    ) : CalendarSideEffect
+}
