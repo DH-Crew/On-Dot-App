f305161c19892e83eb35e4091dd1f400db00cbec
dogmania <hj296914@gmail.com>
2026-06-02 15:43:39 +0900

    ✨ Feat: 반복, 비반복, off 일정 칩 색상 및 border 적용

--- feature/calendar/src/commonMain/kotlin/com/ondot/calendar/ui/component/ScheduleChip.kt
+++ feature/calendar/src/commonMain/kotlin/com/ondot/calendar/ui/component/ScheduleChip.kt
@@ -1,6 +1,7 @@
 package com.ondot.calendar.ui.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,11 +14,11 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.ondot.designsystem.components.OnDotText
-import com.ondot.designsystem.theme.OnDotColor.Gray0
-import com.ondot.designsystem.theme.OnDotColor.Gray400
+import com.ondot.designsystem.theme.OnDotColor.Gray300
 import com.ondot.designsystem.theme.OnDotColor.Gray900
-import com.ondot.designsystem.theme.OnDotColor.Green200
+import com.ondot.designsystem.theme.OnDotColor.Green50
 import com.ondot.designsystem.theme.OnDotColor.Green500
+import com.ondot.designsystem.theme.OnDotColor.Green800
 import com.ondot.domain.model.enums.OnDotTextStyle
 
 @Composable
@@ -28,29 +29,38 @@ fun ScheduleChip(
 ) {
     val backgroundColor =
         when {
-            !hasActiveAlarm -> Gray400
+            !hasActiveAlarm -> Gray900
             isRepeat -> Green500
-            else -> Green200
+            else -> Green50
         }
     val textColor =
         when {
-            !hasActiveAlarm -> Gray0
-            else -> Gray900
+            !hasActiveAlarm -> Gray300
+            isRepeat -> Gray900
+            else -> Green800
         }
+    val borderColor =
+        when {
+            !hasActiveAlarm -> Gray300
+            isRepeat -> Green500
+            else -> Green50
+        }
+    val shape = RoundedCornerShape(2.dp)
 
     Box(
         modifier =
             Modifier
                 .fillMaxWidth()
-                .height(16.dp)
-                .clip(RoundedCornerShape(2.dp))
+                .height(13.dp)
+                .clip(shape)
                 .background(backgroundColor)
-                .padding(horizontal = 2.dp, vertical = 1.dp),
+                .border(1.dp, borderColor, shape)
+                .padding(horizontal = 2.dp),
         contentAlignment = Alignment.Center,
     ) {
         OnDotText(
             text = text,
-            style = OnDotTextStyle.BodySmallR2,
+            style = OnDotTextStyle.BodySmallM,
             color = textColor,
             maxLines = 1,
             overflow = TextOverflow.Ellipsis,
