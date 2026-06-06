5ce23db6de5866ad286c323e760698115e3300d1
dogmania <hj296914@gmail.com>
2026-06-07 01:51:03 +0900

    ✨ Feat: 바텀시트 활성화, 알람 타입 설정 Intent 정의

--- feature/general/src/commonMain/kotlin/com/ondot/general/contract/GeneralScheduleIntent.kt
+++ feature/general/src/commonMain/kotlin/com/ondot/general/contract/GeneralScheduleIntent.kt
@@ -1,6 +1,7 @@
 package com.ondot.general.contract
 
 import com.ondot.domain.model.enums.RouterType
+import com.ondot.domain.model.enums.TimeType
 import com.ondot.domain.model.enums.TransportType
 import com.ondot.domain.model.member.AddressInfo
 import com.ondot.domain.model.member.PlaceHistory
@@ -86,6 +87,16 @@ sealed interface GeneralScheduleIntent : Intent {
 
     data class SetBottomSheetVisible(
         val visible: Boolean,
+    ) : GeneralScheduleIntent
+
+    data class SetAlarmTimeBottomSheet(
+        val type: TimeType?,
+    ) : GeneralScheduleIntent
+
+    data class UpdateAlarmTime(
+        val type: TimeType,
+        val date: LocalDate,
+        val time: LocalTime,
     ) : GeneralScheduleIntent
 
     data class CreateSchedule(
