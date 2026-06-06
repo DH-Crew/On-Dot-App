322be64cbb8d229ad1c5929ea46cd7932761e69d
dogmania <hj296914@gmail.com>
2026-06-06 22:45:17 +0900

    ✨ Feat: PlacePicker, RouteLoading 네비게이션 로직 구현

--- feature/edit/src/commonMain/kotlin/com/ondot/edit/navigation/EditScheduleNavGraph.kt
+++ feature/edit/src/commonMain/kotlin/com/ondot/edit/navigation/EditScheduleNavGraph.kt
@@ -1,5 +1,6 @@
 package com.ondot.edit.navigation
 
+import androidx.compose.runtime.remember
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -7,9 +8,13 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import androidx.navigation.navigation
 import androidx.navigation.toRoute
+import com.ondot.edit.EditPlacePickerRoute
 import com.ondot.edit.EditScheduleScreen
+import com.ondot.edit.EditScheduleViewModel
 import com.ondot.navigation.NavRoutes
 import com.ondot.navigation.base.NavGraphContributor
+import com.ondot.ui.screen.loading.RouteLoadingScreen
+import org.koin.compose.viewmodel.koinViewModel
 
 object EditScheduleNavGraph : NavGraphContributor {
     override val graphRoute: NavRoutes
@@ -26,12 +31,54 @@ object EditScheduleNavGraph : NavGraphContributor {
                 NavRoutes.EditSchedule.ROUTE,
                 arguments = listOf(navArgument("scheduleId") { type = NavType.LongType }),
             ) { backStackEntry ->
+                val parentEntry =
+                    remember(backStackEntry) {
+                        navController.getBackStackEntry(graphRoute.route)
+                    }
+                val viewModel: EditScheduleViewModel = koinViewModel(viewModelStoreOwner = parentEntry)
                 val args = backStackEntry.toRoute<NavRoutes.EditSchedule>()
                 val scheduleId = args.scheduleId
 
                 EditScheduleScreen(
                     scheduleId = scheduleId,
+                    viewModel = viewModel,
                     popScreen = { navController.popBackStack() },
+                    navigateToPlacePicker = {
+                        navController.navigate(NavRoutes.EditPlacePicker.route) {
+                            launchSingleTop = true
+                        }
+                    },
+                    navigateToRouteLoading = {
+                        navController.navigate(NavRoutes.EditRouteLoading.route) {
+                            launchSingleTop = true
+                        }
+                    },
+                )
+            }
+
+            composable(NavRoutes.EditPlacePicker.route) { backStackEntry ->
+                val parentEntry =
+                    remember(backStackEntry) {
+                        navController.getBackStackEntry(graphRoute.route)
+                    }
+                val viewModel: EditScheduleViewModel = koinViewModel(viewModelStoreOwner = parentEntry)
+
+                EditPlacePickerRoute(
+                    viewModel = viewModel,
+                    popScreen = { navController.popBackStack() },
+                    navigateToRouteLoading = {
+                        navController.navigate(NavRoutes.EditRouteLoading.route) {
+                            launchSingleTop = true
+                        }
+                    },
+                )
+            }
+
+            composable(NavRoutes.EditRouteLoading.route) {
+                RouteLoadingScreen(
+                    navigateToNext = {
+                        navController.popBackStack(NavRoutes.EditSchedule.ROUTE, inclusive = false)
+                    },
                 )
             }
         }
