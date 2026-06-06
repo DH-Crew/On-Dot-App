01779aee67f163cc9d9c9b2c7fce2a0dce7dc834
dogmania <hj296914@gmail.com>
2026-06-06 22:46:11 +0900

    ✨ Feat: PlacePickerState 상태 추가

--- feature/edit/src/commonMain/kotlin/com/ondot/edit/EditScheduleUiState.kt
+++ feature/edit/src/commonMain/kotlin/com/ondot/edit/EditScheduleUiState.kt
@@ -4,6 +4,7 @@ import com.ondot.domain.model.enums.TimeBottomSheet
 import com.ondot.domain.model.enums.TimeType
 import com.ondot.domain.model.schedule.ScheduleDetail
 import com.ondot.ui.base.UiState
+import com.ondot.ui.screen.placepicker.model.PlacePickerUiModel
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
@@ -29,4 +30,12 @@ data class EditScheduleUiState
         val showDateBottomSheet: Boolean = false,
         val activeTimeBottomSheet: TimeBottomSheet? = null,
         val selectedTimeType: TimeType = TimeType.DEPARTURE, // 시간을 수정하는 바텀시트가 어떤 것에 의해 렌더링되었는지 저장
-    ) : UiState
+        val placePickerState: PlacePickerUiModel = PlacePickerUiModel(),
+        val isInitialPlacePicker: Boolean = true,
+        val isHomeAddressInitialized: Boolean = false,
+    ) : UiState {
+        val isPlacePickerButtonEnabled: Boolean
+            get() =
+                placePickerState.selectedDeparturePlace != null &&
+                    placePickerState.selectedArrivalPlace != null
+    }
