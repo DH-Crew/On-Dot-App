edb62e16affd9e38c73c358e64e9c557456a8d29
dogmania <hj296914@gmail.com>
2026-06-20 20:22:15 +0900

    ✨ Feat: OnboardingSideEffect 정의

---
+++ feature/onboarding/src/commonMain/kotlin/com/ondot/onboarding/contract/OnboardingSideEffect.kt
@@ -0,0 +1,11 @@
+package com.ondot.onboarding.contract
+
+import com.ondot.domain.model.enums.ToastType
+import com.ondot.ui.base.mvi.SideEffect
+
+sealed interface OnboardingSideEffect : SideEffect {
+    data class ShowToast(
+        val message: String,
+        val type: ToastType,
+    ) : OnboardingSideEffect
+}
