435556f1a93009535f43df18bd8b5302fe532688
dogmania <hj296914@gmail.com>
2026-06-21 22:55:48 +0900

    ♻️ Refactor: 온보딩으로 진입 시 scheduleTitle 초기값 변경

--- feature/general/src/commonMain/kotlin/com/ondot/general/contract/GeneralScheduleViewModel.kt
+++ feature/general/src/commonMain/kotlin/com/ondot/general/contract/GeneralScheduleViewModel.kt
@@ -6,6 +6,7 @@ import com.dh.ondot.presentation.ui.theme.ERROR_GET_HOME_ADDRESS
 import com.dh.ondot.presentation.ui.theme.ERROR_GET_PLACE_HISTORY
 import com.dh.ondot.presentation.ui.theme.ERROR_GET_SCHEDULE_ALARMS
 import com.dh.ondot.presentation.ui.theme.ERROR_SEARCH_PLACE
+import com.dh.ondot.presentation.ui.theme.WORD_COMMUTE
 import com.ondot.domain.model.enums.RouterType
 import com.ondot.domain.model.enums.TimeType
 import com.ondot.domain.model.enums.ToastType
@@ -118,7 +119,12 @@ class GeneralScheduleViewModel(
     }
 
     private fun setOnboardingEntry(isFromOnboarding: Boolean) {
-        reduce { copy(isFromOnboarding = isFromOnboarding) }
+        reduce {
+            copy(
+                isFromOnboarding = isFromOnboarding,
+                scheduleTitle = if (isFromOnboarding) WORD_COMMUTE else currentState.scheduleTitle,
+            )
+        }
     }
 
     private fun toggleRepeat(newValue: Boolean) {
